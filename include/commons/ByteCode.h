c2988f448975fa3e9af6ef4f33d725088ebfe568
Baptiste Wicht <baptistewicht@gmail.com>
2011-06-08 22:32:35 -0700

    Use the new designed library

--- include/commons/ByteCode.h
+++ include/commons/ByteCode.h
@@ -12,25 +12,17 @@ enum ByteCode {
 	END = 2
 };
 
+/* Write operations */
+
 void writeOneOperandCall(ofstream* outStream, ByteCode bytecode, string litteral);
 void writeSimpleCall(ofstream* outStream, ByteCode bytecode);
 void writeEnd(ofstream* stream);
+void writeLitteral(ofstream* stream, string value);
+
+/* Read operations */
 
 ByteCode readByteCode(ifstream* stream);
-
-template<typename T>
-std::ostream& binary_write(std::ostream* stream, const T& value){
-	return stream->write(reinterpret_cast<const char*>(&value), sizeof(T));
-}
-
-template<typename T>
-std::istream & binary_read(std::istream* stream, T& value)
-{
-    return stream->read(reinterpret_cast<char*>(&value), sizeof(T));
-}
-
-std::ostream& binary_write_string(std::ostream* stream, string value);
-
-std::string binary_read_string(std::istream* stream);
+char readConstantType(ifstream* stream);
+string readLitteral(ifstream* stream);
 
 #endif
