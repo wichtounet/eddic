9d8555a145403e84f8312cc91c4949ba01d69bb8
Baptiste Wicht <baptistewicht@gmail.com>
2011-06-19 18:05:07 -0700

    Remove swap files

--- src/ByteCodeFileWriter.cpp
+++ src/ByteCodeFileWriter.cpp
@@ -50,7 +50,6 @@ void ByteCodeFileWriter::writeEnd(){
 }
 
 void ByteCodeFileWriter::writeLitteral(string litteral){
-	binary_write(&stream, 'S');
 	binary_write(&stream, litteral.length() - 2);
 	binary_write_string(&stream, litteral.substr(1, litteral.length() - 2));
 }
