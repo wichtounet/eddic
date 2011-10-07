7e08a62539ee5a61f100521d70fe580d9d279db3
Baptiste Wicht <baptistewicht@gmail.com>
2011-10-07 11:01:01 +0200

    Move function definitions to source files

--- include/Position.hpp
+++ include/Position.hpp
@@ -25,28 +25,14 @@ class Position {
         const std::string m_name;
 
     public:
-        Position(PositionType type, int offset) : m_type(type), m_offset(offset), m_name("") {}
-        Position(PositionType type, std::string name) : m_type(type), m_offset(0), m_name(name) {}
+        Position(PositionType type, int offset);
+        Position(PositionType type, std::string name);
 
-        bool isStack(){
-            return m_type == STACK;
-        }
-
-        bool isParameter(){
-            return m_type == PARAMETER;
-        }
-
-        bool isGlobal(){
-            return m_type == GLOBAL;
-        }
-
-        int offset(){
-            return m_offset;
-        }
-
-        std::string name(){
-            return m_name;
-        }
+        bool isStack();
+        bool isParameter();
+        bool isGlobal();
+        int offset();
+        std::string name();
 };
 
 } //end of eddic
