dca6dc76df618894ef786baa592203321fbd93de
Baptiste Wicht <baptistewicht@gmail.com>
2011-10-07 11:40:30 +0200

    move constructors definitions to source

--- include/Context.hpp
+++ include/Context.hpp
@@ -25,31 +25,31 @@ namespace eddic {
 class Value;
 class Variable;
 
-typedef std::unordered_map<int, std::shared_ptr<Variable>> StoredVariables;
-typedef std::unordered_map<std::string, int> VisibleVariables;
-
 class Context {
     private:
         std::shared_ptr<Context> m_parent;
-        
+
     protected:
+        typedef std::unordered_map<int, std::shared_ptr<Variable>> StoredVariables;
+        typedef std::unordered_map<std::string, int> VisibleVariables;
+
         StoredVariables m_stored;
         VisibleVariables m_visibles;
-        
+
         static int currentVariable;
 
     public:
         Context(std::shared_ptr<Context> parent);
-        
+
         virtual std::shared_ptr<Variable> addVariable(const std::string& a, Type type) = 0;
         virtual bool exists(const std::string& a) const;
         virtual std::shared_ptr<Variable> getVariable(const std::string& variable) const;
         virtual std::shared_ptr<Variable> getVariable(int index) const;
-        
+
         virtual void write(AssemblyFileWriter& writer);
         virtual void release(AssemblyFileWriter& writer);
         std::shared_ptr<Context> parent() const ;
-        
+
         void storeVariable(int index, std::shared_ptr<Variable> variable);
 };
 
