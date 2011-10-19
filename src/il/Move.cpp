403004a5d1a7e40548574fc740594353f145e1c4
Baptiste Wicht <baptistewicht@gmail.com>
2011-10-19 17:27:58 +0200

    Improve Move to have not several operands in memory

--- src/il/Move.cpp
+++ src/il/Move.cpp
@@ -17,8 +17,15 @@ using namespace eddic;
 Move::Move(std::shared_ptr<Operand> lhs, std::shared_ptr<Operand> rhs) : m_lhs(lhs), m_rhs(rhs) {}
 
 void Move::write(AssemblyFileWriter& writer){
-    //We can always put an immediate value everywhere
-    writer.stream() << "movl " << m_lhs->getValue() << ", " << m_rhs->getValue() << std::endl;
+    //Cannot move stack to stack directly, must use a register
+    if(m_lhs->isStack() && m_rhs->isStack()){
+        //TODO Avoid using eax if not available
+        writer.stream() << "movl " << m_lhs->getValue() << ", %eax" << std::endl;
+        writer.stream() << "movl %eax, " << m_rhs->getValue() << std::endl;
+    } else {
+        //We can always put an immediate value everywhere
+        writer.stream() << "movl " << m_lhs->getValue() << ", " << m_rhs->getValue() << std::endl;
+    }
 
     //TODO Improve ?
 }
