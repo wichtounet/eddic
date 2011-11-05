36849bf574d5b143e408cc776c777a7c09591752
Baptiste Wicht <baptistewicht@gmail.com>
2011-11-05 22:41:25 +0100

    Compile swap

--- src/IntermediateCompiler.cpp
+++ src/IntermediateCompiler.cpp
@@ -13,8 +13,11 @@
 #include "VisitorUtils.hpp"
 
 #include "Context.hpp"
+#include "Variable.hpp"
+#include "SemanticalException.hpp"
 
 #include "il/IntermediateProgram.hpp"
+#include "il/Operands.hpp"
 
 #include "ast/Program.hpp"
 
@@ -43,13 +46,79 @@ class CompilerVisitor : public boost::static_visitor<> {
         void operator()(ASTFunctionDeclaration& function){
             program.addInstruction(program.factory().createFunctionDeclaration(function.mangledName, function.context->size()));
 
-            //visit_each(*this, function.instructions);
+            visit_each(*this, function.instructions);
 
             program.addInstruction(program.factory().createFunctionExit(function.context->size()));
         }
 
         void operator()(GlobalVariableDeclaration& variable){
             //Nothing to compile, the global variable values are written using global contexts
+        }
+
+        void operator()(ASTIf& if_){
+            //TODO
+        }
+
+        void operator()(ASTAssignment& assignment){
+            //TODO
+        }
+
+        void operator()(ASTDeclaration& declaration){
+            //TODO
+        }
+
+        void operator()(ASTSwap& swap){
+            //We have the guarantee here that both variables are of the same type
+            switch (swap.lhs_var->type()) {
+                case Type::INT:{
+                    auto registerA = createRegisterOperand("eax");
+                    auto registerB = createRegisterOperand("ebx");
+               
+                    program.addInstruction(program.factory().createMove(swap.lhs_var->toIntegerOperand(), registerA));
+                    program.addInstruction(program.factory().createMove(swap.rhs_var->toIntegerOperand(), registerB));
+
+                    program.addInstruction(program.factory().createMove(registerB, swap.lhs_var->toIntegerOperand()));
+                    program.addInstruction(program.factory().createMove(registerA, swap.rhs_var->toIntegerOperand()));
+
+                    break;
+                }
+                case Type::STRING:{
+                    auto registerA = createRegisterOperand("eax");
+                    auto registerB = createRegisterOperand("ebx");
+                    auto registerC = createRegisterOperand("ecx");
+                    auto registerD = createRegisterOperand("edx");
+                    
+                    program.addInstruction(program.factory().createMove(swap.lhs_var->toStringOperand().first, registerA));
+                    program.addInstruction(program.factory().createMove(swap.lhs_var->toStringOperand().second, registerB));
+                    program.addInstruction(program.factory().createMove(swap.rhs_var->toStringOperand().first, registerC));
+                    program.addInstruction(program.factory().createMove(swap.rhs_var->toStringOperand().second, registerD));
+                    
+                    program.addInstruction(program.factory().createMove(registerC, swap.lhs_var->toStringOperand().first));
+                    program.addInstruction(program.factory().createMove(registerD, swap.lhs_var->toStringOperand().second));
+                    program.addInstruction(program.factory().createMove(registerA, swap.rhs_var->toStringOperand().first));
+                    program.addInstruction(program.factory().createMove(registerB, swap.rhs_var->toStringOperand().second));
+
+                    break;
+                }
+                default:
+                   throw SemanticalException("Variable of invalid type");
+            }
+        }
+
+        void operator()(ASTFor for_){
+            //TODO
+        }
+
+        void operator()(ASTWhile& while_){
+            //TODO
+        }
+
+        void operator()(ASTForeach& foreach){
+            //TODO
+        }
+
+        void operator()(ASTFunctionCall& functionCall){
+            //TODO
         }
 };
 
