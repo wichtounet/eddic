2d22922468f1ef56f7cdeb8e70fcd04f96838de3
Baptiste Wicht <baptistewicht@gmail.com>
2011-11-20 22:00:52 +0100

    Fix the allocator

--- src/il/AllocFunction.cpp
+++ src/il/AllocFunction.cpp
@@ -24,21 +24,51 @@ void AllocFunction::write(AssemblyFileWriter& writer){
         << "cmpl %ebx, %ecx" << std::endl
         << "jle alloc_normal" << std::endl
 
-        //Alloc new block
+        //Get the current address
         << "movl $45, %eax" << std::endl        //45 = sys_brk
-        << "movl $16384, %ebx" << std::endl     //alloc 16K
+        << "xorl %ebx, %ebx" << std::endl       //get end
+        << "int  $0x80" << std::endl
+       
+        //%eax is the current address 
+        << "movl %eax, %esi" << std::endl
+
+        //Alloc new block of 16384K from the current address
+        << "movl %eax, %ebx" << std::endl
+        << "addl $16384, %ebx" << std::endl
+        << "movl $45, %eax" << std::endl        //45 = sys_brk
         << "int  $0x80" << std::endl
 
-        << "movl $16384, VIeddi_remaining" << std::endl
-        << "movl %eax, VIeddi_current" << std::endl
+        //zero'd the new block
+        << "movl %eax, %edi" << std::endl       //edi = start of block
 
-        << "jmp alloc_end" << std::endl
+        << "subl $4, %edi" << std::endl         //EDI points to the last DWORD available to us
+        << "movl $4096, %ecx" << std::endl         //this many DWORDs were allocated
+        << "xorl %eax, %eax"  << std::endl         //   ; will write with zeroes
+        << "std"  << std::endl         //        ; walk backwards
+        << "rep stosl"  << std::endl         //      ; write all over the reserved area
+        << "cld"  << std::endl         //        ; bring back the DF flag to normal state
+
+        << "movl %esi, %eax" << std::endl
+
+        //We now have 16K of available memory starting at %esi
+        << "movl $16384, VIeddi_remaining" << std::endl
+        << "movl %esi, VIeddi_current" << std::endl
 
         << "alloc_normal:" << std::endl
 
         //old = current
         << "movl VIeddi_current, %eax" << std::endl
-
+        
+        //current += size
+        << "movl VIeddi_current, %ebx" << std::endl
+        << "addl %ecx, %ebx" << std::endl
+        << "movl %ebx, VIeddi_current" << std::endl
+        
+        //remaining -= size
+        << "movl VIeddi_remaining, %ebx" << std::endl
+        << "subl %ecx, %ebx" << std::endl
+        << "movl %ebx, VIeddi_remaining" << std::endl
+       
         << "alloc_end:" << std::endl
 
         << "leave" << std::endl
