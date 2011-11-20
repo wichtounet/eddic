a07d24b7c47bf35b32f8bad95a7a6e847f5af06f
Baptiste Wicht <baptistewicht@gmail.com>
2011-11-20 21:01:27 +0100

    Implement the eddi_malloc function

--- src/il/AllocFunction.cpp
+++ src/il/AllocFunction.cpp
@@ -18,9 +18,29 @@ void AllocFunction::write(AssemblyFileWriter& writer){
         << "pushl %ebp" << std::endl
         << "movl %esp, %ebp" << std::endl
 
-        << "movl 8(%ebp), %eax" << std::endl
+        << "movl 8(%ebp), %ecx" << std::endl
+        << "movl VIeddi_remaining, %ebx" << std::endl
+
+        << "cmpl %ebx, %ecx" << std::endl
+        << "jle alloc_normal" << std::endl
+
+        //Alloc new block
+        << "movl $45, %eax" << std::endl        //45 = sys_brk
+        << "movl $16384, %ebx" << std::endl     //alloc 16K
+        << "int  $0x80" << std::endl
+
+        << "movl $16384, VIeddi_remaining" << std::endl
+        << "movl %eax, VIeddi_current" << std::endl
+
+        << "jmp alloc_end" << std::endl
+
+        << "alloc_normal:" << std::endl
+
+        //old = current
+        << "movl VIeddi_current, %eax" << std::endl
+
+        << "alloc_end:" << std::endl
 
         << "leave" << std::endl
         << "ret" << std::endl;
-    
 }
