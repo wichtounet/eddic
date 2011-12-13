0c3754bb56db009c918fab57fb317fb527ca0da1
Baptiste Wicht <baptistewicht@gmail.com>
2011-12-13 16:22:18 +0100

    Extract basic blocks from tac statements

--- src/tac/BasicBlockExtractor.cpp
+++ src/tac/BasicBlockExtractor.cpp
@@ -6,6 +6,9 @@
 //=======================================================================
 
 #include <string>
+#include <unordered_map>
+
+#include <boost/variant.hpp>
 
 #include "tac/BasicBlockExtractor.hpp"
 #include "tac/Program.hpp"
@@ -18,7 +21,34 @@ namespace {
 } //end of anonymous namespace
 
 void tac::BasicBlockExtractor::extract(tac::Program& program) const {
+    std::unordered_map<std::string, std::shared_ptr<BasicBlock>> labels;
+
     for(auto& function : program.functions){
-        
+        auto current = function->newBasicBlock();
+
+        bool nextIsLeader = false;
+
+        for(auto& statement : function->getStatements()){
+            if(auto* ptr = boost::get<std::string>(&statement)){
+                function->newBasicBlock();
+
+                labels[*ptr] = function->currentBasicBlock();
+
+                nextIsLeader = false;
+            } else {
+                if(nextIsLeader){
+                    function->newBasicBlock();
+                    nextIsLeader = false;
+                }
+
+                if(boost::get<tac::IfFalse>(&statement) || boost::get<tac::Return>(&statement) || boost::get<tac::Goto>(&statement)){
+                    nextIsLeader = true;
+                }
+
+                function->currentBasicBlock()->add(statement);
+            }
+        }
+
+        function->getStatements().clear();
     }
 }
