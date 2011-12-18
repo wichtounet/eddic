cf649eaf6f35a8c2b318c9d9cb8649a59cf0e06b
Baptiste Wicht <baptistewicht@gmail.com>
2011-12-18 19:24:20 +0100

    Review order in initialization list

--- src/tac/Quadruple.cpp
+++ src/tac/Quadruple.cpp
@@ -13,4 +13,4 @@ tac::Quadruple::Quadruple(){}
 tac::Quadruple::Quadruple(std::shared_ptr<Variable> r, tac::Argument arg) : result(r), arg1(arg) {}
 tac::Quadruple::Quadruple(std::shared_ptr<Variable> r, tac::Argument a1, Operator o, tac::Argument a2) : result(r), arg1(a1), arg2(a2), op(o) {}
 
-tac::Quadruple::Quadruple(Operator o, tac::Argument a) : op(o), arg1(a) {}
+tac::Quadruple::Quadruple(Operator o, tac::Argument a) : arg1(a), op(o) {}
