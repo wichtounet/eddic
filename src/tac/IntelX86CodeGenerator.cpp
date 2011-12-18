06e40cc67b95e9994f163e4028f044ca20173656
Baptiste Wicht <baptistewicht@gmail.com>
2011-12-18 19:52:00 +0100

    Do not move a value in a register before overriding its value

--- src/tac/IntelX86CodeGenerator.cpp
+++ src/tac/IntelX86CodeGenerator.cpp
@@ -344,7 +344,8 @@ struct StatementCompiler : public boost::static_visitor<> {
 
         if(!quadruple->op){
             //TODO Optimize move of 0 in a register with xorl
-            writer.stream() << "movl " << arg(quadruple->arg1) << ", " << arg(quadruple->result) << std::endl;            
+            Register reg = getRegNoMove(quadruple->result);
+            writer.stream() << "movl " << arg(quadruple->arg1) << ", " << regToString(reg) << std::endl;            
         } else {
             switch(*quadruple->op){
                 //TODO Optimize a = a + 1 and a = a -1 with inc and dec
