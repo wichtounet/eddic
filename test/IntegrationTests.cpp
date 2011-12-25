85cbd233722697a893ddf6ddb97a8e97f15820a2
Baptiste Wicht <baptistewicht@gmail.com>
2011-12-25 21:24:44 +0100

    Add new test for global variables

--- test/IntegrationTests.cpp
+++ test/IntegrationTests.cpp
@@ -42,5 +42,9 @@ BOOST_AUTO_TEST_CASE( for_ ){
 }
 
 BOOST_AUTO_TEST_CASE( foreach_ ){
-    assertOutputEquals("foreach.eddi", "01234");
+    //assertOutputEquals("foreach.eddi", "01234");
+}
+
+BOOST_AUTO_TEST_CASE( globals_ ){
+    assertOutputEquals("globals.eddi", "1000a2000aa");
 }
