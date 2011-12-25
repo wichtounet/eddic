f7ea982078ba5159b78ee68aa5e3b6ed83f64e6e
Baptiste Wicht <baptistewicht@gmail.com>
2011-12-25 22:08:06 +0100

    Introduce the structure of the optimizer

--- src/tac/Optimizer.cpp
+++ src/tac/Optimizer.cpp
@@ -5,10 +5,36 @@
 //  http://www.boost.org/LICENSE_1_0.txt)
 //=======================================================================
 
+#include <boost/variant.hpp>
+
 #include "tac/Optimizer.hpp"
+#include "tac/Program.hpp"
 
 using namespace eddic;
 
+namespace {
+
+struct ArithmeticIdentities : public boost::static_visitor<tac::Statement> {
+    template<typename T>
+    tac::Statement operator()(T& statement) const { 
+        return statement;
+    }
+};
+    
+}
+
+template<typename Visitor>
+void apply_to_all(Visitor visitor, tac::Program& program){
+    for(auto& function : program.functions){
+        for(auto& block : function->getBasicBlocks()){
+            for(auto& statement : block->statements){
+                statement = boost::apply_visitor(visitor, statement);
+            }
+        }
+    }
+}
+
 void tac::Optimizer::optimize(tac::Program& program) const {
-    //TODO
+    ArithmeticIdentities identities;
+    apply_to_all(identities, program);
 }
