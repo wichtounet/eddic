9003c94a8941581b29772a11b196d571763f6167
Baptiste Wicht <baptistewicht@gmail.com>
2011-12-29 22:53:24 +0100

    Handle minus and plus nodes

--- src/TypeChecker.cpp
+++ src/TypeChecker.cpp
@@ -39,6 +39,7 @@ struct CheckerVisitor : public boost::static_visitor<> {
     AUTO_RECURSE_SIMPLE_LOOPS()
     AUTO_RECURSE_BRANCHES()
     AUTO_RECURSE_BINARY_CONDITION()
+    AUTO_RECURSE_MINUS_PLUS_VALUES()
    
     void operator()(ast::FunctionDeclaration& declaration){
         visit_each(*this, declaration.Content->instructions);
