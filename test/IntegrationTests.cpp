870fc5e4697dd64ee27a60eccbb42ca3e8f47679
Baptiste Wicht <baptistewicht@gmail.com>
2012-01-04 15:41:53 +0100

    Compiles all the samples in the unit tests

--- test/IntegrationTests.cpp
+++ test/IntegrationTests.cpp
@@ -15,19 +15,63 @@
 #define BOOST_TEST_MODULE Eddic Tests
 #include <boost/test/unit_test.hpp>
 
-void assertOutputEquals(const std::string& file, const std::string& output){
+void assertCompiles(const std::string& file){
     eddic::parseOptions(0, {});
 
     eddic::Compiler compiler;
 
-    int code = compiler.compileOnly("test/cases/" + file);
+    int code = compiler.compileOnly(file);
 
     BOOST_CHECK_EQUAL (code, 0);
+}
+
+void assertOutputEquals(const std::string& file, const std::string& output){
+    assertCompiles("test/cases/" + file);
 
     std::string out = eddic::execCommand("./a.out"); 
     
     BOOST_CHECK_EQUAL (output, out);
 }
+
+/* Compiles all the samples */
+
+BOOST_AUTO_TEST_CASE( samples_arrays ){
+    assertCompiles("samples/arrays.eddi");
+}
+
+BOOST_AUTO_TEST_CASE( samples_asm ){
+    assertCompiles("samples/asm.eddi");
+}
+
+BOOST_AUTO_TEST_CASE( samples_assembly ){
+    assertCompiles("samples/assembly.eddi");
+}
+
+BOOST_AUTO_TEST_CASE( samples_const ){
+    assertCompiles("samples/const.eddi");
+}
+
+BOOST_AUTO_TEST_CASE( samples_concat ){
+    assertCompiles("samples/concat.eddi");
+}
+
+BOOST_AUTO_TEST_CASE( samples_functions ){
+    assertCompiles("samples/functions.eddi");
+}
+
+BOOST_AUTO_TEST_CASE( samples_includes ){
+    assertCompiles("samples/includes.eddi");
+}
+
+BOOST_AUTO_TEST_CASE( samples_optimize ){
+    assertCompiles("samples/optimize.eddi");
+}
+
+BOOST_AUTO_TEST_CASE( samples_problem ){
+    assertCompiles("samples/problem.eddi");
+}
+
+/* Specific tests */ 
 
 BOOST_AUTO_TEST_CASE( if_ ){
     assertOutputEquals("if.eddi", "Cool");
