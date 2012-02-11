91ed2024bbd231be0ff957492dfef75bbc06b8ab
Baptiste Wicht <baptistewicht@gmail.com>
2012-02-11 20:47:54 +0100

    Add source for both generators

---
+++ src/asm/IntelX86CodeGenerator.cpp
@@ -0,0 +1,12 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#include "asm/IntelX86CodeGenerator.hpp"
+
+using namespace eddic;
+
+as::IntelX86CodeGenerator::IntelX86CodeGenerator(AssemblyFileWriter& w) : CodeGenerator(w) {}
