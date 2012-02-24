88a3e5cc441e67b274cd5d990b9ca5fee49d8acf
Baptiste Wicht <baptistewicht@gmail.com>
2012-02-24 22:22:39 +0100

    Add relational operators to int

--- src/ast/TypeChecker.cpp
+++ src/ast/TypeChecker.cpp
@@ -197,7 +197,7 @@ struct CheckerVisitor : public boost::static_visitor<> {
             
             if(type.base() == BaseType::INT){
                 if(op != ast::Operator::DIV && op != ast::Operator::MUL && op != ast::Operator::SUB && op != ast::Operator::ADD && op != ast::Operator::MOD &&
-                        op != ast::Operator::GREATER && op != ast::Operator::GREATER_EQUALS && op != ast::Operator::LESS && op != ast::Operator::LESS_EQUALS &&
+                    op != ast::Operator::GREATER && op != ast::Operator::GREATER_EQUALS && op != ast::Operator::LESS && op != ast::Operator::LESS_EQUALS &&
                         op != ast::Operator::EQUALS && op != ast::Operator::NOT_EQUALS){
                     throw SemanticalException("The " + ast::toString(op) + " operator cannot be applied on int");
                 }
