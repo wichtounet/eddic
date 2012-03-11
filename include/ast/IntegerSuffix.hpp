58e423078fddbbee28d4216de59ce6342069d6b8
Baptiste Wicht <baptistewicht@gmail.com>
2012-03-11 15:52:16 +0100

    Add a new node to the AST for suffixed integers

---
+++ include/ast/IntegerSuffix.hpp
@@ -0,0 +1,39 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef AST_INTEGER_SUFFIX_H
+#define AST_INTEGER_SUFFIX_H
+
+#include <boost/fusion/include/adapt_struct.hpp>
+
+#include "ast/TerminalNode.hpp"
+
+namespace eddic {
+
+namespace ast {
+
+/*!
+ * \class IntegerSuffix
+ * \brief The AST node for an integer suffixed to be another type.    
+ */
+struct IntegerSuffix : public TerminalNode {
+    int value;
+    std::string suffix;
+};
+
+} //end of ast
+
+} //end of eddic
+
+//Adapt the struct for the AST
+BOOST_FUSION_ADAPT_STRUCT(
+    eddic::ast::Integer, 
+    (int, value)
+    (std::string, suffix)
+)
+
+#endif
