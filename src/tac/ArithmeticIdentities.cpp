b37501842aca947acffce2e48591f35c626117b1
Baptiste Wicht <baptistewicht@gmail.com>
2012-03-11 18:16:02 +0100

    Apply arithmetic identities to float

--- src/tac/ArithmeticIdentities.cpp
+++ src/tac/ArithmeticIdentities.cpp
@@ -75,6 +75,69 @@ void tac::ArithmeticIdentities::operator()(std::shared_ptr<tac::Quadruple>& quad
                 }
 
                 break;
+            case tac::Operator::FADD:
+                if(*quadruple->arg1 == 0.0){
+                    replaceRight(*this, quadruple, *quadruple->arg2, tac::Operator::FASSIGN);
+                } else if(*quadruple->arg2 == 0.0){
+                    replaceRight(*this, quadruple, *quadruple->arg1, tac::Operator::FASSIGN);
+                }
+
+                break;
+            case tac::Operator::FSUB:
+                if(*quadruple->arg2 == 0.0){
+                    replaceRight(*this, quadruple, *quadruple->arg1, tac::Operator::FASSIGN);
+                } 
+
+                //a = b - b => a = 0
+                else if(*quadruple->arg1 == *quadruple->arg2){
+                    replaceRight(*this, quadruple, 0.0, tac::Operator::FASSIGN);
+                }
+                
+                //a = 0 - b => a = -b
+                else if(*quadruple->arg1 == 0.0){
+                    replaceRight(*this, quadruple, *quadruple->arg2, tac::Operator::FMINUS);
+                }
+
+                break;
+            case tac::Operator::FMUL:
+                if(*quadruple->arg1 == 1.0){
+                    replaceRight(*this, quadruple, *quadruple->arg2, tac::Operator::FASSIGN);
+                } else if(*quadruple->arg2 == 1.0){
+                    replaceRight(*this, quadruple, *quadruple->arg1, tac::Operator::FASSIGN);
+                }
+                
+                else if(*quadruple->arg1 == 0.0){
+                    replaceRight(*this, quadruple, 0.0, tac::Operator::FASSIGN);
+                } else if(*quadruple->arg2 == 0){
+                    replaceRight(*this, quadruple, 0.0, tac::Operator::FASSIGN);
+                }
+                
+                else if(*quadruple->arg1 == -1.0){
+                    replaceRight(*this, quadruple, *quadruple->arg2, tac::Operator::FMINUS);
+                } else if(*quadruple->arg2 == -1.0){
+                    replaceRight(*this, quadruple, *quadruple->arg1, tac::Operator::FMINUS);
+                }
+
+                break;
+            case tac::Operator::FDIV:
+                if(*quadruple->arg2 == 1.0){
+                    replaceRight(*this, quadruple, *quadruple->arg1, tac::Operator::FASSIGN);
+                }
+
+                else if(*quadruple->arg1 == 0.0){
+                    replaceRight(*this, quadruple, 0.0, tac::Operator::FASSIGN);
+                }
+
+                //a = b / b => a = 1
+                else if(*quadruple->arg1 == *quadruple->arg2){
+                    replaceRight(*this, quadruple, 1.0, tac::Operator::FASSIGN);
+                }
+                
+                else if(*quadruple->arg2 == 1.0){
+                    replaceRight(*this, quadruple, *quadruple->arg1, tac::Operator::MINUS);
+                }
+
+                break;
             default:
                 break;
         }
