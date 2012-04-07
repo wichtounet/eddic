995be6ebc291b9c62aa02ecc9c29529396608608
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-04-07 15:37:20 +0200

    Add a node for a member declaration

---
+++ include/ast/MemberDeclaration.hpp
@@ -0,0 +1,56 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef AST_MEMBER_DECLARATION_H
+#define AST_MEMBER_DECLARATION_H
+
+#include <boost/fusion/include/adapt_struct.hpp>
+
+#include "ast/Deferred.hpp"
+#include "ast/Position.hpp"
+
+namespace eddic {
+
+class Context;
+
+namespace ast {
+
+/*!
+ * \class ASTMemberDeclaration
+ * \brief The AST node for a declaration of a member variable. 
+ * Should only be used from the Deferred version (eddic::ast::MemberDeclaration).
+ */
+struct ASTMemberDeclaration {
+    std::shared_ptr<Context> context;
+
+    Position position;
+    std::string variableType;
+    std::string variableName;
+
+    mutable long references;
+    ASTMemberDeclaration() : references(0) {}
+};
+
+/*!
+ * \typedef MemberDeclaration
+ * \brief The AST node for a declaration of a member variable. 
+ */
+typedef Deferred<ASTMemberDeclaration> MemberDeclaration;
+
+} //end of ast
+
+} //end of eddic
+
+//Adapt the struct for the AST
+BOOST_FUSION_ADAPT_STRUCT(
+    eddic::ast::MemberDeclaration, 
+    (eddic::ast::Position, Content->position)
+    (std::string, Content->variableType)
+    (std::string, Content->variableName)
+)
+
+#endif
