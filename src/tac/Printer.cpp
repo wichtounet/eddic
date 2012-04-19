9ea947be0adefd88040abb4810ed9f60164f35f0
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-04-19 19:12:29 +0200

    Fix compilation on Release mode

--- src/tac/Printer.cpp
+++ src/tac/Printer.cpp
@@ -5,6 +5,7 @@
 //  http://www.boost.org/LICENSE_1_0.txt)
 //=======================================================================
 
+#include <iostream>
 #include <memory>
 
 #include <boost/variant.hpp>
