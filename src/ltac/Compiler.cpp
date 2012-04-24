ec330f029d40d3b1e021c3038ec2d87a49db5553
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-04-24 13:24:37 +0200

    Use helper functions to simplify the compiler code

--- src/ltac/Compiler.cpp
+++ src/ltac/Compiler.cpp
@@ -11,6 +11,14 @@
 
 using namespace eddic;
 
+void add_instruction(std::shared_ptr<ltac::Function> function, ltac::Operator op, ltac::Argument arg1){
+    function->add(std::make_shared<ltac::Instruction>(op, arg1));
+}
+
+void add_instruction(std::shared_ptr<ltac::Function> function, ltac::Operator op, ltac::Argument arg1, ltac::Argument arg2){
+    function->add(std::make_shared<ltac::Instruction>(op, arg1, arg2));
+}
+
 void ltac::Compiler::compile(std::shared_ptr<mtac::Program> source, std::shared_ptr<ltac::Program> target) const {
     for(auto& src_function : source->functions){
         auto target_function = std::make_shared<ltac::Function>(src_function->context, src_function->getName());
@@ -26,7 +34,7 @@ void ltac::Compiler::compile(std::shared_ptr<mtac::Function> src_function, std::
     
     //Only if necessary, allocates size on the stack for the local variables
     if(size > 0){
-        target_function->add(std::make_shared<ltac::Instruction>(ltac::Operator::ALLOC_STACK, size));
+        add_instruction(target_function, ltac::Operator::ALLOC_STACK, size);
     }
     
     auto iter = src_function->context->begin();
@@ -37,12 +45,12 @@ void ltac::Compiler::compile(std::shared_ptr<mtac::Function> src_function, std::
         if(var->type().isArray() && var->position().isStack()){
             int position = -var->position().offset();
 
-            target_function->add(std::make_shared<ltac::Instruction>(ltac::Operator::MOV, ltac::Address(ltac::BP, position), var->type().size()));
+            add_instruction(target_function, ltac::Operator::MOV, ltac::Address(ltac::BP, position), var->type().size());
 
             if(var->type().base() == BaseType::INT){
-                target_function->add(std::make_shared<ltac::Instruction>(ltac::Operator::MEMSET, ltac::Address(ltac::BP, position, -8), var->type().size()));
+                add_instruction(target_function, ltac::Operator::MEMSET, ltac::Address(ltac::BP, position, -8), var->type().size());
             } else if(var->type().base() == BaseType::STRING){
-                target_function->add(std::make_shared<ltac::Instruction>(ltac::Operator::MEMSET, ltac::Address(ltac::BP, position, -8), 2 * var->type().size()));
+                add_instruction(target_function, ltac::Operator::MEMSET, ltac::Address(ltac::BP, position, -8), 2 * var->type().size());
             }
         }
     }
@@ -52,6 +60,6 @@ void ltac::Compiler::compile(std::shared_ptr<mtac::Function> src_function, std::
     
     //Only if necessary, deallocates size on the stack for the local variables
     if(size > 0){
-        target_function->add(std::make_shared<ltac::Instruction>(ltac::Operator::FREE_STACK, size));
+        add_instruction(target_function, ltac::Operator::FREE_STACK, size);
     }
 }
