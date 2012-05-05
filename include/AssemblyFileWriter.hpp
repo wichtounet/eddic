e380d85a75df829d233c8dea63db1c6c59cd26d4
Baptiste Wicht <baptistewicht@gmail.com>
2012-05-05 18:56:18 +0300

    Doxygen

--- include/AssemblyFileWriter.hpp
+++ include/AssemblyFileWriter.hpp
@@ -16,17 +16,36 @@ namespace eddic {
 /*!
  * \class AssemblyFileWriter
  * \brief A simple writer to append assembly code to the assembly output file. 
+ * This class give access to its internal buffer that is only outputted to the file 
+ * when the write() function is called. 
  */
 class AssemblyFileWriter {
     private:
         std::ofstream m_stream;
         std::stringstream buffer;
 	
-    public:		
+    public:
+    	/**
+    	 * Construct an AssemblyFileWriter that will output on the given path. 
+    	 * \param path The path to the file. 
+    	 */
         AssemblyFileWriter(const std::string& path);
+        
+        /**
+         * Close the file stream. 
+         */
         ~AssemblyFileWriter();
         
+        /**
+         * \brief Return the enclosing stream. 
+         * This method should be used to output assembly instruction to the file. 
+         * \return A reference to the enclosing stream. 
+         */
         std::stringstream& stream();
+        
+        /**
+         * Output the buffer to the file. 
+         */
         void write();
 };
 
