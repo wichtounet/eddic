b6191ec6716e699a668927d872c7f6aa1ade6000
Baptiste Wicht <baptistewicht@gmail.com>
2012-05-06 22:54:14 +0300

    Update include/asm/CodeGenerator.hpp

--- include/asm/CodeGenerator.hpp
+++ include/asm/CodeGenerator.hpp
@@ -19,11 +19,23 @@ class AssemblyFileWriter;
 
 namespace as {
 
+/*!
+ * \class CodeGenerator
+ * Generates code for the backend from the LTAC Program. 
+ */
 class CodeGenerator {
     public:
+        /*!
+         * Constructs a CodeGenerator. 
+         * \param The AssemblyFileWriter to use to output code to. 
+         */
         CodeGenerator(AssemblyFileWriter& writer);
         
-        //Entry point
+        /*!
+         * Generates codes from the LTAC Program. 
+         * \param program The source LTAC Program. 
+         * \param pool The string pool to use. 
+         */
         virtual void generate(ltac::Program& program, StringPool& pool) = 0;
 
     protected:
