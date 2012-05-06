ecee66021ff4a919b6d3c012c410b047be917c54
Baptiste Wicht <baptistewicht@gmail.com>
2012-05-06 23:05:44 +0300

    Update include/asm/IntelAssemblyUtils.hpp

--- include/asm/IntelAssemblyUtils.hpp
+++ include/asm/IntelAssemblyUtils.hpp
@@ -17,10 +17,22 @@ class AssemblyFileWriter;
 
 namespace as {
 
+/*!
+ * Save the given registers on the stack. Works only for int registers. 
+ * \param The writer to use to output the instructions
+ * \param registers The registers that are to be saved on the stack. 
+ */
 void save(AssemblyFileWriter& writer, const std::vector<std::string>& registers);
+
+/*!
+ * Restore the given registers from the stack. Works only for int registers. They are restored in their reverse 
+ * order so that the same list can be used to save and restore the registers. 
+ * \param The writer to use to output the instructions
+ * \param registers The registers that are to be restored from the stack. 
+ */
 void restore(AssemblyFileWriter& writer, const std::vector<std::string>& registers);
 
-} //end of mtac
+} //end of as
 
 } //end of eddic
 
