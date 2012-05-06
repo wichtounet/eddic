884d63bc129741e50762a8dee1aa88c8bf2728d8
Baptiste Wicht <baptistewicht@gmail.com>
2012-05-06 23:27:42 +0300

    Update include/asm/Registers.hpp

--- include/asm/Registers.hpp
+++ include/asm/Registers.hpp
@@ -19,6 +19,13 @@ class Variable;
 
 namespace as {
 
+/*!
+ * \class Registers
+ * \brief Manager for registers. 
+ * This class handles the management of the registers for a low-level compiler. 
+ * This class holds the free registers, the locations of the variables and reservations of registers. 
+ * \param Reg The type of register to handle. 
+ */
 template<typename Reg>
 class Registers {
     typedef typename std::vector<Reg>::const_iterator Iterator;
@@ -27,35 +34,121 @@ class Registers {
     public:
         Registers(std::vector<Reg> registers, std::shared_ptr<Variable> var);
 
+        /*!
+         * Deleted copy constructor
+         */
         Registers(const Registers<Reg>& rhs) = delete;
+        
+        /*!
+         * Deleted copy assignment operator. 
+         */
         Registers& operator=(const Registers<Reg>& rhs) = delete;
-
+    
+        /*!
+         * Reset the content of the registers manager. Only the list of available registers is kept. 
+         */
         void reset();
 
+        /*!
+         * Indicates if the given variable is in a register. 
+         * \param variable The variable to test. 
+         * \return true if the variable is hold in a register, otherwise false. 
+         */
         bool inRegister(std::shared_ptr<Variable> variable);
+        
+        /*!
+         * Indicates if the given variable is in the given register. 
+         * \param variable The variable to test. 
+         * \param reg The register to test. 
+         * \return true if the variable is hold in the given register, otherwise false. 
+         */
         bool inRegister(std::shared_ptr<Variable> variable, Reg reg);
+        
+        /*!
+         * Indicates if the given register is used. 
+         * \param reg The register to test. 
+         * \return true if the register is used, otherwise false. 
+         */
         bool used(Reg reg);
 
+        /*!
+         * Reserve the given register. A reserved register cannot be used to hold a variable. 
+         * \param reg The register to reserve. 
+         */
         void reserve(Reg reg);
+
+        /*!
+         * Release the given reserved register. 
+         * \param reg The register to release. 
+         */
         void release(Reg reg);
+        
+        /*!
+         * Indicates if the given register is reserved. 
+         * \param reg The register to test. 
+         * \return true if the register is reserved, otherwise false. 
+         */
         bool reserved(Reg reg);
 
+        /*!
+         * Return the register in which the variable is hold. 
+         * \param The variable to test. 
+         * \return The register in which the variable is hold.
+         */
         Reg operator[](const std::shared_ptr<Variable> variable);
+
+        /*!
+         * Return the variable hold in the given register. 
+         * \param The register to test. 
+         * \return The variable hold in the given register.
+         */
         std::shared_ptr<Variable> operator[](Reg reg);
 
+        /*!
+         * Return the first register. 
+         * \return The first register.
+         */
         Reg first() const ;
 
-        void setLocation(std::shared_ptr<Variable> variable, Reg a);
+        /*!
+         * Set the location of the given variable to the given register. 
+         * \param variable The variable to set the location for. 
+         * \param reg The register that holds the variable. 
+         */
+        void setLocation(std::shared_ptr<Variable> variable, Reg reg);
+        
+        /*!
+         * Remove the variable from the variable that holds it. 
+         * \param variable The variable to remove from the register. 
+         */
         void remove(std::shared_ptr<Variable> variable);
 
+        /*!
+         * Returns an iterator to the first register. 
+         * \return An iterator pointing to the first register. 
+         */
         Iterator begin() const;
+        
+        /*!
+         * Returns an iterator to the last register. 
+         * \return An iterator pointing past the last register. 
+         */
         Iterator end() const;
         
+        /*!
+         * Returns an iterator to the last register. 
+         * \return An iterator pointing to the last register. 
+         */
         ReverseIterator rbegin() const;
+        
+        /*!
+         * Returns an iterator to the first register. 
+         * \return An iterator pointing past the first register. 
+         */
         ReverseIterator rend() const;
 
     private:
-        std::vector<Reg> registers;   
+        std::vector<Reg> registers;
         std::vector<std::shared_ptr<Variable>> descriptors;
         std::unordered_map<std::shared_ptr<Variable>, Reg> variables;
 
