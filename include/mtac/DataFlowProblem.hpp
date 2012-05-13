555b7f14b637eb2941a24e36141d81be6a12de80
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-05-13 17:54:30 +0200

    Prepare definitions for global data flow frameworks

---
+++ include/mtac/DataFlowProblem.hpp
@@ -0,0 +1,35 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef MTAC_DataOPTIMIZER_H
+#define MTAC_OPTIMIZER_H
+
+#include "mtac/Program.hpp"
+
+namespace eddic {
+
+class StringPool;
+
+namespace mtac {
+
+template<Domain>
+struct DataFlowProblem {
+    //What about statement ?
+    //What about reading the basic blocks ?
+
+    Domain meet(Domain& values);
+    Domain transfer(Domain& values);
+
+    void Boundary(Values& values);
+    void Init(Values& values);
+}
+
+} //end of mtac
+
+} //end of eddic
+
+#endif
---
+++ include/mtac/GlobalOptimizations.hpp
@@ -0,0 +1,23 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef MTAC_GLOBAL_OPTIMIZATIONS_H
+#define MTAC_GLOBAL_OPTIMIZATIONS_H
+
+#include "mtac/Program.hpp"
+
+namespace eddic {
+
+namespace mtac {
+
+void forward_data_flow();
+
+} //end of mtac
+
+} //end of eddic
+
+#endif
