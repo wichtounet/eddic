ee6514d3c3fd0e6c334023fbfb9bc73ffafd9e12
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-06-02 16:11:30 +0200

    Fix warnings

--- include/mtac/GlobalOptimizations.hpp
+++ include/mtac/GlobalOptimizations.hpp
@@ -134,7 +134,7 @@ std::shared_ptr<DataFlowResults<mtac::Domain<DomainValues>>>  backward_data_flow
 }
 
 template<DataFlowType Type, typename DomainValues>
-std::shared_ptr<DataFlowResults<mtac::Domain<DomainValues>>> basic_data_flow(std::shared_ptr<mtac::Function> function, DataFlowProblem<Type, DomainValues>& problem){
+std::shared_ptr<DataFlowResults<mtac::Domain<DomainValues>>> basic_data_flow(std::shared_ptr<mtac::Function>/* function*/, DataFlowProblem<Type, DomainValues>&/* problem*/){
     typedef mtac::Domain<DomainValues> Domain;
 
     auto results = std::make_shared<DataFlowResults<Domain>>();
