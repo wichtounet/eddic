d436debe0f47b55ae2ba04032a5c2eece5a4b03d
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-06-09 19:13:40 +0200

    Use new_type instead of new_array_type

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -141,13 +141,13 @@ void SymbolTable::defineStandardFunctions(){
     //time function
     auto timeFunction = std::make_shared<Function>(VOID, "time");
     timeFunction->mangledName = "_F4timeAI";
-    timeFunction->parameters.push_back({"a", new_array_type(BaseType::INT)});
+    timeFunction->parameters.push_back({"a", new_type("int[]")});
     addFunction(timeFunction);
     
     //duration function
     auto durationFunction = std::make_shared<Function>(VOID, "duration");
     durationFunction->mangledName = "_F8durationAIAI";
-    durationFunction->parameters.push_back({"a", new_array_type(BaseType::INT)});
-    durationFunction->parameters.push_back({"b", new_array_type(BaseType::INT)});
+    durationFunction->parameters.push_back({"a", new_type("int[]")});
+    durationFunction->parameters.push_back({"b", new_type("int[]")});
     addFunction(durationFunction);
 }
