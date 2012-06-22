23ceaba955ae1a0fb04c01942500b032d67f28e0
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-06-22 19:35:10 +0200

    Fix the script, not all statements were counted

--- tools/stats.sh
+++ tools/stats.sh
@@ -14,20 +14,25 @@ cases_dir="$base_dir/test/cases/*.eddi"
 #This sample should be avoided for performance reasons
 big_sample="$base_dir/samples/big.eddi"
 
-#Count LTAC Statements
+#Count Statements in samples
 for file in $sample_dir ; do
     if [ "$file" != "$big_sample" ] ; then
         count="`$executable --quiet --ltac-only $file | wc -l`"
         let "count_ltac += $count"
+    
+        count="`$executable --quiet --mtac-only $file | wc -l`"
+        let "count_mtac += $count"
     fi
 done
 
-echo "Total LTAC Statements: $count_ltac"
-
-#Count MTAC Statements
+#Count Statements in test cases
 for file in $cases_dir ; do
+    count="`$executable --quiet --ltac-only $file | wc -l`"
+    let "count_ltac += $count"
+    
     count="`$executable --quiet --mtac-only $file | wc -l`"
     let "count_mtac += $count"
 done
 
 echo "Total MTAC Statements: $count_mtac"
+echo "Total LTAC Statements: $count_ltac"
