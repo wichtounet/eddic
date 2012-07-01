5c845b4af012c423761746f51cf61c02e36238cc
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-07-01 21:42:28 +0200

    Improve the optimization model for the peephole optimizer

--- src/ltac/PeepholeOptimizer.cpp
+++ src/ltac/PeepholeOptimizer.cpp
@@ -22,7 +22,7 @@ inline bool is_reg(T value){
     return mtac::is<ltac::Register>(value);
 }
 
-inline void optimize_statement(ltac::Statement& statement){
+inline bool optimize_statement(ltac::Statement& statement){
     if(boost::get<std::shared_ptr<ltac::Instruction>>(&statement)){
         auto instruction = boost::get<std::shared_ptr<ltac::Instruction>>(statement);
 
@@ -32,7 +32,7 @@ inline void optimize_statement(ltac::Statement& statement){
                 instruction->op = ltac::Operator::XOR;
                 instruction->arg2 = instruction->arg1;
 
-                return;
+                return true;
             }
 
             if(is_reg(*instruction->arg1) && is_reg(*instruction->arg2)){
@@ -44,6 +44,8 @@ inline void optimize_statement(ltac::Statement& statement){
                     instruction->op = ltac::Operator::NOP;
                     instruction->arg1.reset();
                     instruction->arg2.reset();
+                    
+                    return true;
                 }
             }
         }
@@ -54,7 +56,7 @@ inline void optimize_statement(ltac::Statement& statement){
                 instruction->op = ltac::Operator::INC;
                 instruction->arg2.reset();
 
-                return;
+                    return true;
             }
             
             //ADD reg, -1 can be transformed into DEC reg
@@ -62,7 +64,7 @@ inline void optimize_statement(ltac::Statement& statement){
                 instruction->op = ltac::Operator::DEC;
                 instruction->arg2.reset();
 
-                return;
+                    return true;
             }
         }
         
@@ -72,7 +74,7 @@ inline void optimize_statement(ltac::Statement& statement){
                 instruction->op = ltac::Operator::DEC;
                 instruction->arg2.reset();
 
-                return;
+                    return true;
             }
             
             //SUB reg, -1 can be transformed into INC reg
@@ -80,7 +82,7 @@ inline void optimize_statement(ltac::Statement& statement){
                 instruction->op = ltac::Operator::INC;
                 instruction->arg2.reset();
 
-                return;
+                    return true;
             }
         }
 
@@ -95,28 +97,28 @@ inline void optimize_statement(ltac::Statement& statement){
                     instruction->op = ltac::Operator::SHIFT_LEFT;
                     instruction->arg2 = powerOfTwo(constant);
 
-                    return;
+                    return true;
                 } 
                 
                 if(constant == 3){
                     instruction->op = ltac::Operator::LEA;
                     instruction->arg2 = ltac::Address(reg, reg, 2, 0);
 
-                    return;
+                    return true;
                 } 
                 
                 if(constant == 5){
                     instruction->op = ltac::Operator::LEA;
                     instruction->arg2 = ltac::Address(reg, reg, 4, 0);
 
-                    return;
+                    return true;
                 } 
                 
                 if(constant == 9){
                     instruction->op = ltac::Operator::LEA;
                     instruction->arg2 = ltac::Address(reg, reg, 8, 0);
 
-                    return;
+                    return true;
                 } 
             }
         }
@@ -127,13 +129,15 @@ inline void optimize_statement(ltac::Statement& statement){
                 instruction->op = ltac::Operator::OR;
                 instruction->arg2 = instruction->arg1;
 
-                return;
+                    return true;
             }
         }
     }
+
+    return false;
 }
 
-inline void multiple_statement_optimizations(ltac::Statement& s1, ltac::Statement& s2){
+inline bool multiple_statement_optimizations(ltac::Statement& s1, ltac::Statement& s2){
     if(mtac::is<std::shared_ptr<ltac::Instruction>>(s1) && mtac::is<std::shared_ptr<ltac::Instruction>>(s2)){
         auto& i1 = boost::get<std::shared_ptr<ltac::Instruction>>(s1);
         auto& i2 = boost::get<std::shared_ptr<ltac::Instruction>>(s2);
@@ -141,13 +145,18 @@ inline void multiple_statement_optimizations(ltac::Statement& s1, ltac::Statemen
         //The seconde LEAVE is dead
         if(i1->op == ltac::Operator::LEAVE && i2->op == ltac::Operator::LEAVE){
             i2->op = ltac::Operator::NOP;
+            
+            return true;
         }
 
         //Combine two FREE STACK into one
         if(i1->op == ltac::Operator::FREE_STACK && i2->op == ltac::Operator::FREE_STACK){
             i1->arg1 = boost::get<int>(*i1->arg1) + boost::get<int>(*i2->arg1);
-            i2->arg1.reset();
+            
             i2->op = ltac::Operator::NOP;
+            i2->arg1.reset();
+            
+            return true;
         }
 
         if(i1->op == ltac::Operator::MOV && i2->op == ltac::Operator::MOV){
@@ -160,6 +169,8 @@ inline void multiple_statement_optimizations(ltac::Statement& s1, ltac::Statemen
                 //cross MOV (ir4 = ir5, ir5 = ir4), keep only the first
                 if (reg11 == reg22 && reg12 == reg21){
                     i2->op = ltac::Operator::NOP;
+                    
+                    return true;
                 }
             } else if(is_reg(*i1->arg1) && is_reg(*i2->arg1)){
                 auto reg11 = boost::get<ltac::Register>(*i1->arg1);
@@ -168,6 +179,8 @@ inline void multiple_statement_optimizations(ltac::Statement& s1, ltac::Statemen
                 //Two MOV to the same register => keep only last MOV
                 if(reg11 == reg21){
                     i1->op = ltac::Operator::NOP;
+                    
+                    return true;
                 }
             } else if(is_reg(*i1->arg1) && is_reg(*i2->arg2)){
                 if(boost::get<ltac::Address>(&*i1->arg2) && boost::get<ltac::Address>(&*i2->arg1)){
@@ -175,6 +188,8 @@ inline void multiple_statement_optimizations(ltac::Statement& s1, ltac::Statemen
                         i2->op = ltac::Operator::NOP;
                         i2->arg1.reset();
                         i2->arg2.reset();
+                        
+                        return true;
                     }
                 }
             } else if(is_reg(*i1->arg2) && is_reg(*i2->arg1)){
@@ -183,6 +198,8 @@ inline void multiple_statement_optimizations(ltac::Statement& s1, ltac::Statemen
                         i2->op = ltac::Operator::NOP;
                         i2->arg1.reset();
                         i2->arg2.reset();
+                        
+                        return true;
                     }
                 }
             }
@@ -198,6 +215,8 @@ inline void multiple_statement_optimizations(ltac::Statement& s1, ltac::Statemen
                         i1->op = ltac::Operator::NOP;
                         i1->arg1.reset();
                         i1->arg2.reset();
+                        
+                        return true;
                     } else if(boost::get<std::string>(&*i1->arg2) && boost::get<int>(&*i2->arg2)){
                         i2->op = ltac::Operator::LEA;
                         i2->arg2 = ltac::Address(boost::get<std::string>(*i1->arg2), boost::get<int>(*i2->arg2));
@@ -205,11 +224,15 @@ inline void multiple_statement_optimizations(ltac::Statement& s1, ltac::Statemen
                         i1->op = ltac::Operator::NOP;
                         i1->arg1.reset();
                         i1->arg2.reset();
+                        
+                        return true;
                     }
                 }
             }
         }
     }
+
+    return false;
 }
 
 inline bool is_nop(ltac::Statement& statement){
@@ -224,21 +247,23 @@ inline bool is_nop(ltac::Statement& statement){
     return false;
 }
 
-void basic_optimizations(std::shared_ptr<ltac::Function> function){
+bool basic_optimizations(std::shared_ptr<ltac::Function> function){
     auto& statements = function->getStatements();
 
     auto it = statements.begin();
     auto end = statements.end() - 1;
+
+    bool optimized = false;
 
     while(it != end){
         auto& s1 = *it;
         auto& s2 = *(it + 1);
 
         //Optimizations that looks at only one statement
-        optimize_statement(s1);
+        optimized |= optimize_statement(s1);
 
         //Optimizations that looks at several statements at once
-        multiple_statement_optimizations(s1, s2);
+        optimized |= multiple_statement_optimizations(s1, s2);
 
         if(is_nop(s1)){
             it = statements.erase(it);
@@ -249,15 +274,19 @@ void basic_optimizations(std::shared_ptr<ltac::Function> function){
 
         ++it;
     }
+
+    return optimized;
 }
 
 void eddic::ltac::optimize(std::shared_ptr<ltac::Program> program){
     PerfsTimer("Peephole optimizations", true);
 
-    //TODO Make something comparable to the optimization model for MTAC
-    for(int i = 0; i < 2; ++i){
-        for(auto& function : program->functions){
-            basic_optimizations(function);
-        }
+    for(auto& function : program->functions){
+        bool optimized;
+        do {
+            optimized = false;
+            
+            optimized |= basic_optimizations(function);
+        } while(optimized);
     }
 }
