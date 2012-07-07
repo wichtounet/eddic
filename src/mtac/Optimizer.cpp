74aec3f0f390fd2a8a82485753b72f23214d57b0
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-07-07 17:24:53 +0200

    Move the merge of basic bloks out of the optimizer

--- src/mtac/Optimizer.cpp
+++ src/mtac/Optimizer.cpp
@@ -44,6 +44,7 @@
 #include "mtac/VariableCleaner.hpp"
 #include "mtac/RemoveEmptyFunctions.hpp"
 #include "mtac/DeadCodeElimination.hpp"
+#include "mtac/MergeBasicBlocks.hpp"
 
 using namespace eddic;
 
@@ -290,64 +291,6 @@ bool remove_needless_jumps(std::shared_ptr<mtac::Function> function){
     return optimized;
 }
 
-bool merge_basic_blocks(std::shared_ptr<mtac::Function> function){
-    bool optimized = false;
-
-    std::unordered_set<std::shared_ptr<mtac::BasicBlock>> usage;
-
-    computeBlockUsage(function, usage);
-
-    auto& blocks = function->getBasicBlocks();
-
-    auto it = blocks.begin();
-
-    //The ENTRY Basic block should not been merged
-    ++it;
-
-    while(it != blocks.end()){
-        auto& block = *it;
-        if(likely(!block->statements.empty())){
-            auto& last = block->statements[block->statements.size() - 1];
-
-            bool merge = false;
-
-            if(boost::get<std::shared_ptr<mtac::Quadruple>>(&last)){
-                merge = true;
-            } else if(auto* ptr = boost::get<std::shared_ptr<mtac::Call>>(&last)){
-                merge = safe(*ptr); 
-            } else if(boost::get<std::shared_ptr<mtac::NoOp>>(&last)){
-                merge = true;
-            }
-
-            auto next = it;
-            ++next;
-
-            if(merge && next != blocks.end() && (*next)->index != -2){
-                //Only if the next block is not used because we will remove its label
-                if(usage.find(*next) == usage.end()){
-                    if(auto* ptr = boost::get<std::shared_ptr<mtac::Call>>(&(*(*next)->statements.begin()))){
-                        if(!safe(*ptr)){
-                            ++it;
-                            continue;
-                        }
-                    }
-
-                    block->statements.insert(block->statements.end(), (*next)->statements.begin(), (*next)->statements.end());
-
-                    it = blocks.erase(next);
-                    optimized = true;
-
-                    --it;
-                    continue;
-                }
-            }
-        }
-
-        ++it;
-    }
-   
-    return optimized; 
-}
 
 template<typename Problem>
 bool data_flow_optimization(std::shared_ptr<mtac::Function> function){
