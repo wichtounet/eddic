4680acb30a078a9e32d819993c14a32e2ba4ccd9
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-07-08 22:06:54 +0200

    First working version of inlining

--- src/mtac/inlining.cpp
+++ src/mtac/inlining.cpp
@@ -9,6 +9,8 @@
 
 #include "SymbolTable.hpp"
 #include "Options.hpp"
+#include "Type.hpp"
+#include "FunctionContext.hpp"
 
 #include "mtac/inlining.hpp"
 
@@ -52,8 +54,84 @@ bool mtac::inline_functions(std::shared_ptr<mtac::Program> program){
         }
     }
 
-    for(auto function : inlined){
-        std::cout << "inline " << function->getName() << std::endl;
+    for(auto source_function : inlined){
+        auto source_definition = source_function->definition;
+       
+        for(auto dest_function : program->functions){
+            if(dest_function == source_function){
+                continue;
+            }
+
+            auto bit = dest_function->getBasicBlocks().begin();
+            auto bend = dest_function->getBasicBlocks().end();
+
+            while(bit != bend){
+                auto basic_block = *bit;
+
+                auto it = basic_block->statements.begin();
+                auto end = basic_block->statements.end();
+
+                while(it != end){
+                    auto statement = *it;
+
+                    if(auto* ptr = boost::get<std::shared_ptr<mtac::Call>>(&statement)){
+                        auto call = *ptr;
+
+                        if(call->functionDefinition == source_definition){
+                            std::cout << "inline " << source_definition->mangledName << " in function " << dest_function->definition->mangledName << std::endl;
+
+                            //Handle parameters
+                            if(source_definition->parameters.size() > 0){
+                                ++it;
+                                continue;//TODO Temporary
+                            }
+                            
+                            //Handle return type
+                            if(source_definition->returnType != VOID){
+                                ++it;
+                                continue;//TODO Temporary
+                            }
+
+                            auto vit = source_definition->context->begin();
+                            auto vend = source_definition->context->end();
+
+                            while(vit != vend){
+                                auto variable = vit->second;
+
+                                std::cout << "extract variable " << variable->name() << std::endl;
+
+                                ++vit;
+                            }
+
+                            auto saved_bit = bit;
+
+                            for(auto block : source_function->getBasicBlocks()){
+                                //Copy all basic blocks except ENTRY and EXIT
+                                if(block->index >= 0){
+                                    auto new_bb = std::make_shared<mtac::BasicBlock>(dest_function->getBasicBlocks().size() + 1);
+                                    new_bb->context = block->context;
+                                    new_bb->statements = block->statements;
+
+                                    bit = dest_function->getBasicBlocks().insert(bit, new_bb);
+                                    bend = dest_function->getBasicBlocks().end();
+                                }
+                            }
+
+                            bit = saved_bit;
+
+                            it = basic_block->statements.erase(it);
+                            end = basic_block->statements.end();
+
+                            continue;
+                        }
+                    }
+
+                    ++it;
+                }
+
+                ++bit;
+            }
+        }
     }
 
     return optimized;
