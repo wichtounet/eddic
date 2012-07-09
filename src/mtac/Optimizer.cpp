f8235787d9d52d06c6eec83537d6c7b27da4ecd3
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-07-09 22:50:24 +0200

    Cleanup

--- src/mtac/Optimizer.cpp
+++ src/mtac/Optimizer.cpp
@@ -189,10 +189,10 @@ void optimize_function(std::shared_ptr<mtac::Function> function, std::shared_ptr
         optimized |= debug("Optimize Concat", &mtac::optimize_concat, function, pool);
         optimized |= debug("Remove dead basic block", &mtac::remove_dead_basic_blocks, function);
         optimized |= debug("Merge basic block", &mtac::merge_basic_blocks, function);
-        
-        optimized |= debug("Dead-Code Elimination", &mtac::dead_code_elimination, function);
 
         remove_nop(function);
+        
+        optimized |= debug("Dead-Code Elimination", &mtac::dead_code_elimination, function);
     } while (optimized);
 
     //Remove variables that are not used after optimizations
