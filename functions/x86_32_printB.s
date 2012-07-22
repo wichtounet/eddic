8b1d2c66451be84b4d7e1c9c7bb1242c29d8d9bc
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-07-22 17:16:05 +0200

    Assembly optimizations

--- functions/x86_32_printB.s
+++ functions/x86_32_printB.s
@@ -4,13 +4,16 @@ mov ebp, esp
 push ecx
 or ecx, ecx
 jne .true_print
+
 xor ecx, ecx
 call _F5printI
-jmp .end
+pop ecx
+leave
+ret
+
 .true_print:
 mov ecx, 1
 call _F5printI
-.end:
 pop ecx
 leave
 ret
--- functions/x86_64_printB.s
+++ functions/x86_64_printB.s
@@ -4,13 +4,16 @@ mov rbp, rsp
 push r14
 or r14, r14
 jne .true_print
+
 xor r14, r14
 call _F5printI
-jmp .end
+pop r14
+leave
+ret
+
 .true_print:
 mov r14, 1
 call _F5printI
-.end:
 pop r14
 leave
 ret
