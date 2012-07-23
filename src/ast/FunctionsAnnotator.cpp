1daecd8481f425605bef2ed9e4a3288162e7b728
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-07-23 17:53:54 +0200

    Recurse into switch cases

--- src/ast/FunctionsAnnotator.cpp
+++ src/ast/FunctionsAnnotator.cpp
@@ -114,6 +114,9 @@ class FunctionCheckerVisitor : public boost::static_visitor<> {
         AUTO_RECURSE_UNARY_VALUES()
         AUTO_RECURSE_VARIABLE_OPERATIONS()
         AUTO_RECURSE_STRUCT()
+        AUTO_RECURSE_SWITCH()
+        AUTO_RECURSE_SWITCH_CASE()
+        AUTO_RECURSE_DEFAULT_CASE()
 
         void operator()(ast::FunctionDeclaration& declaration){
             currentFunction = symbols.getFunction(declaration.Content->mangledName);
