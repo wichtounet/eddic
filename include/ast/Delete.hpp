5da12ca9575f9489a5ab6b0601b69b8452a746cf
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-07-24 11:45:40 +0200

    Prepare AST node for new and delete operators

---
+++ include/ast/Delete.hpp
@@ -0,0 +1,56 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef AST_DELETE_H
+#define AST_DELETE_H
+
+#include <memory>
+#include <string>
+
+#include "ast/Deferred.hpp"
+#include "ast/Position.hpp"
+
+namespace eddic {
+
+class Context;
+class Variable;
+
+namespace ast {
+
+/*!
+ * \class ASTDelete
+ * \brief The AST node for delete a variable. 
+ * Should only be used from the Deferred version (eddic::ast::ArrayValue).
+ */
+struct ASTDelete {
+    std::shared_ptr<Context> context;
+    std::shared_ptr<Variable> var;
+    
+    Position position;
+    std::string variable_name;
+
+    mutable long references = 0;
+};
+
+/*!
+ * \typedef Delete
+ * \brief The AST node for delete a pointer.
+ */
+typedef Deferred<ASTArrayValue> ArrayValue;
+
+} //end of ast
+
+} //end of eddic
+
+//Adapt the struct for the AST
+BOOST_FUSION_ADAPT_STRUCT(
+    eddic::ast::Delete, 
+    (eddic::ast::Position, Content->position)
+    (std::string, Content->variable_name)
+)
+
+#endif
