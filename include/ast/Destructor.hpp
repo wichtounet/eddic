57d6b23db26744305ce796c825ac459d12e593ed
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-07-28 14:09:33 +0200

    Create node for constructors and destructors

---
+++ include/ast/Destructor.hpp
@@ -0,0 +1,53 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef AST_DESTRUCTOR_H
+#define AST_DESTRUCTOR_H
+
+#include <boost/fusion/include/adapt_struct.hpp>
+
+#include "ast/Instruction.hpp"
+#include "ast/Position.hpp"
+
+namespace eddic {
+
+class FunctionContext;
+
+namespace ast {
+
+/*!
+ * \class ASTFunctionDeclaration
+ * \brief The AST node for a function declaration.  
+ * Should only be used from the Deferred version (eddic::ast::FunctionDeclaration).
+ */
+struct ASTDestructor { 
+    std::shared_ptr<FunctionContext> context;
+
+    Position position;
+    std::vector<Instruction> instructions;
+
+    mutable long references = 0;
+};
+
+/*!
+ * \typedef FunctionDeclaration
+ * \brief The AST node for a function declaration.
+ */
+typedef Deferred<ASTDestructor> Destructor; 
+
+} //end of ast
+
+} //end of eddic
+
+//Adapt the struct for the AST
+BOOST_FUSION_ADAPT_STRUCT(
+    eddic::ast::Destructor, 
+    (eddic::ast::Position, Content->position)
+    (std::vector<eddic::ast::Instruction>, Content->instructions)
+)
+
+#endif
