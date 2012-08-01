e996acf6777b4c1b2ed39ecd69c8375a8768d9d8
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-08-02 00:22:20 +0200

    Move symbol table information in the global context

--- src/GlobalContext.cpp
+++ src/GlobalContext.cpp
@@ -49,3 +49,165 @@ std::shared_ptr<Variable> GlobalContext::addVariable(const std::string& variable
     Position position(PositionType::GLOBAL, variable);
     return variables[variable] = std::make_shared<Variable>(variable, type, position, val);
 }
+
+void GlobalContext::addFunction(std::shared_ptr<Function> function){
+    functions[function->mangledName] = function;
+}
+
+std::shared_ptr<Function> GlobalContext::getFunction(const std::string& function){
+    ASSERT(exists(function), "The function must exists");
+
+    return functions[function];
+}
+
+bool GlobalContext::exists(const std::string& function){
+    return functions.find(function) != functions.end();
+}
+
+void GlobalContext::add_struct(std::shared_ptr<Struct> struct_){
+    structs[struct_->name] = struct_;
+}
+
+std::shared_ptr<Struct> GlobalContext::get_struct(const std::string& struct_){
+    return structs[struct_];
+}
+
+int GlobalContext::member_offset(std::shared_ptr<Struct> struct_, const std::string& member){
+    int offset = 0;
+
+    for(auto m : struct_->members){
+        if(m->name == member){
+            return offset;
+        }
+
+        offset += m->type->size();
+    }
+
+    ASSERT_PATH_NOT_TAKEN("The member is not part of the struct");
+}
+
+int GlobalContext::size_of_struct(const std::string& struct_name){
+    int struct_size = 0;
+
+    auto struct_ = get_struct(struct_name);
+
+    for(auto m : struct_->members){
+        struct_size += m->type->size();
+    }
+    
+    return struct_size;
+}
+
+bool GlobalContext::is_recursively_nested(const std::string& struct_name, unsigned int left){
+    if(left == 0){
+        return true;
+    }
+
+    auto struct_ = get_struct(struct_name);
+
+    for(auto m : struct_->members){
+        auto type = m->type;
+
+        if(type->is_custom_type()){
+            if(is_recursively_nested(type->type(), left - 1)){
+                return true;
+            }
+        }
+    }
+    
+    return false;
+}
+
+bool GlobalContext::is_recursively_nested(const std::string& struct_){
+    return is_recursively_nested(struct_, 100);
+}
+
+bool GlobalContext::struct_exists(const std::string& struct_){
+    return structs.find(struct_) != structs.end();
+}
+
+void GlobalContext::addReference(const std::string& function){
+    ASSERT(exists(function), "The function must exists");
+    
+    ++(functions[function]->references);
+}
+
+void GlobalContext::removeReference(const std::string& function){
+    ASSERT(exists(function), "The function must exists");
+    
+    --(functions[function]->references);
+}
+
+int GlobalContext::referenceCount(const std::string& function){
+    ASSERT(exists(function), "The function must exists");
+    
+    return functions[function]->references;
+}
+
+void GlobalContext::addPrintFunction(const std::string& function, std::shared_ptr<const Type> parameterType){
+    auto printFunction = std::make_shared<Function>(VOID, "print");
+    printFunction->standard = true;
+    printFunction->mangledName = function;
+    printFunction->parameters.push_back({"a", parameterType});
+    addFunction(printFunction);
+}
+
+void GlobalContext::defineStandardFunctions(){
+    auto printLineFunction = std::make_shared<Function>(VOID, "print");
+    printLineFunction->standard = true;
+    printLineFunction->mangledName = "_F7println";
+    addFunction(printLineFunction);
+
+    //print string
+    addPrintFunction("_F5printS", STRING);
+    addPrintFunction("_F7printlnS", STRING);
+
+    //print integer
+    addPrintFunction("_F5printI", INT);
+    addPrintFunction("_F7printlnI", INT);
+
+    //print bool
+    addPrintFunction("_F5printB", BOOL);
+    addPrintFunction("_F7printlnB", BOOL);
+
+    //print float
+    addPrintFunction("_F5printF", FLOAT);
+    addPrintFunction("_F7printlnF", FLOAT);
+    
+    //concat function
+    auto concatFunction = std::make_shared<Function>(STRING, "concat");
+    concatFunction->standard = true;
+    concatFunction->mangledName = "_F6concatSS";
+    concatFunction->parameters.push_back({"a", STRING});
+    concatFunction->parameters.push_back({"b", STRING});
+    addFunction(concatFunction);
+    
+    //alloc function
+    auto allocFunction = std::make_shared<Function>(new_pointer_type(INT), "alloc");
+    allocFunction->standard = true;
+    allocFunction->mangledName = "_F5allocI";
+    allocFunction->parameters.push_back({"a", INT});
+    addFunction(allocFunction);
+    
+    //free function
+    auto freeFunction = std::make_shared<Function>(VOID, "free");
+    freeFunction->standard = true;
+    freeFunction->mangledName = "_F4freePI";
+    freeFunction->parameters.push_back({"a", INT});
+    addFunction(freeFunction);
+    
+    //time function
+    auto timeFunction = std::make_shared<Function>(VOID, "time");
+    timeFunction->standard = true;
+    timeFunction->mangledName = "_F4timeAI";
+    timeFunction->parameters.push_back({"a", new_type("int[]")});
+    addFunction(timeFunction);
+    
+    //duration function
+    auto durationFunction = std::make_shared<Function>(VOID, "duration");
+    durationFunction->standard = true;
+    durationFunction->mangledName = "_F8durationAIAI";
+    durationFunction->parameters.push_back({"a", new_type("int[]")});
+    durationFunction->parameters.push_back({"b", new_type("int[]")});
+    addFunction(durationFunction);
+}
