b491d50391fa2fd9a4aba75d533420f38ba7565f
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-08-02 15:02:09 +0200

    Fix size computation

--- src/ast/StructuresAnnotator.cpp
+++ src/ast/StructuresAnnotator.cpp
@@ -36,7 +36,24 @@ struct StructuresCollector : public boost::static_visitor<> {
         }
 
         auto signature = std::make_shared<Struct>(struct_.Content->name);
+        context->add_struct(signature);
+    }
+
+    AUTO_IGNORE_OTHERS()
+};
+
+struct StructureMembersCollector : public boost::static_visitor<> {
+    std::shared_ptr<GlobalContext> context;
+
+    StructureMembersCollector(std::shared_ptr<GlobalContext> context) : context(context) {}
+
+    AUTO_RECURSE_PROGRAM()
+
+    void operator()(ast::Struct& struct_){
+        auto signature = context->get_struct(struct_.Content->name);
         std::vector<std::string> names;
+
+        signature->members.clear();
 
         for(auto& member : struct_.Content->members){
             if(std::find(names.begin(), names.end(), member.Content->name) != names.end()){
@@ -53,8 +70,6 @@ struct StructuresCollector : public boost::static_visitor<> {
 
             signature->members.push_back(std::make_shared<Member>(member.Content->name, member_type));
         }
-
-        context->add_struct(signature);
     }
 
     AUTO_IGNORE_OTHERS()
@@ -91,6 +106,10 @@ struct StructuresVerifier : public boost::static_visitor<> {
 void ast::defineStructures(ast::SourceFile& program){
     StructuresCollector collector(program.Content->context);
     collector(program);
+    
+    StructureMembersCollector member_collector(program.Content->context);
+    member_collector(program);
+    member_collector(program);
     
     StructuresVerifier verify(program.Content->context);
     verify(program);
