a0ee20d2ce2930073b308f31bdf91c2c4f9d54b0
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-08-26 21:07:39 +0200

    Test validity of parameter types

--- src/ast/VariablesAnnotator.cpp
+++ src/ast/VariablesAnnotator.cpp
@@ -74,10 +74,34 @@ struct VariablesVisitor : public boost::static_visitor<> {
         visit_each_non_variant(*this, struct_.Content->functions);
     }
 
+    bool is_valid(const std::string& type){
+        if(is_standard_type(type)){
+            return true;
+        }
+
+        return context->struct_exists(type);
+    }
+
+    bool is_valid(const ast::Type& type){
+        if(auto* ptr = boost::get<ast::ArrayType>(&type)){
+            return is_valid(ptr->type);
+        } else if(auto* ptr = boost::get<ast::SimpleType>(&type)){
+            return is_valid(ptr->type);
+        } else if(auto* ptr = boost::get<ast::PointerType>(&type)){
+            return is_valid(ptr->type);
+        }
+
+        ASSERT_PATH_NOT_TAKEN("Invalid type");
+    }
+
     template<typename Function>
     void visit_function(Function& declaration){
         //Add all the parameters to the function context
         for(auto& parameter : declaration.Content->parameters){
+            if(is_valid(parameter.parameterType)){
+                throw SemanticalException("Invalid parameter type", declaration.Content->position);
+            }
+
             auto type = visit(ast::TypeTransformer(context), parameter.parameterType);
             
             declaration.Content->context->addParameter(parameter.parameterName, type);    
