293db17744c9eec657f951113437a8e9555f0d0e
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-09-09 16:35:14 +0200

    Prepare the optimization

---
+++ src/mtac/loop_optimizations.cpp
@@ -0,0 +1,16 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#include "mtac/loop_optimizations.hpp"
+
+using namespace eddic;
+
+bool mtac::loop_invariant_code_motion(std::shared_ptr<mtac::Function> function){
+    //TODO
+    
+    return false;
+}
