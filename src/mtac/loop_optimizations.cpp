3e7badbcf4d928cbcf3dade2b35e2ec3544d3630
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-09-09 19:29:25 +0200

    Validate invariant

--- src/mtac/loop_optimizations.cpp
+++ src/mtac/loop_optimizations.cpp
@@ -7,7 +7,10 @@
 
 #include <unordered_map>
 
+#include <boost/graph/dominator_tree.hpp>
+
 #include "iterators.hpp"
+#include "VisitorUtils.hpp"
 
 #include "mtac/loop_optimizations.hpp"
 #include "mtac/loop_analysis.hpp"
@@ -20,6 +23,9 @@ namespace {
 
 typedef mtac::ControlFlowGraph::InternalControlFlowGraph G;
 typedef std::set<mtac::ControlFlowGraph::BasicBlockInfo> Loop;
+typedef mtac::ControlFlowGraph::BasicBlockInfo Vertex;
+typedef boost::property_map<mtac::ControlFlowGraph::InternalControlFlowGraph, boost::vertex_index_t>::type IndexMap;
+typedef boost::iterator_property_map<std::vector<Vertex>::iterator, IndexMap> PredMap;
 
 struct Usage {
     std::unordered_map<std::shared_ptr<Variable>, unsigned int> written;
@@ -90,13 +96,114 @@ std::shared_ptr<mtac::BasicBlock> create_pre_header(const Loop& loop, std::share
 
         ++bit;
     }
-
-    //TODO There are certainly cases when this is not enough
     
     return pre_header;
 }
 
-bool loop_invariant_code_motion(const Loop& loop, std::shared_ptr<mtac::Function> function, const G& g){
+struct UsageCollector : public boost::static_visitor<bool> {
+    std::shared_ptr<Variable> var;
+
+    UsageCollector(std::shared_ptr<Variable> var) : var(var) {}
+
+    template<typename T>
+    bool collect(T& arg){
+        if(auto* variablePtr = boost::get<std::shared_ptr<Variable>>(&arg)){
+            return *variablePtr == var;
+        }
+
+        return false;
+    }
+
+    template<typename T>
+    bool collect_optional(T& opt){
+        if(opt){
+            return collect(*opt);
+        }
+
+        return false;
+    }
+
+    bool operator()(std::shared_ptr<mtac::Quadruple> quadruple){
+        return quadruple->result == var || collect_optional(quadruple->arg1) || collect_optional(quadruple->arg2);
+    }
+    
+    bool operator()(std::shared_ptr<mtac::Param> param){
+        return collect(param->arg);
+    }
+    
+    bool operator()(std::shared_ptr<mtac::If> if_){
+        return collect(if_->arg1) || collect_optional(if_->arg2);
+    }
+    
+    bool operator()(std::shared_ptr<mtac::IfFalse> if_false){
+        return collect(if_false->arg1) || collect_optional(if_false->arg2);
+    }
+
+    template<typename T>
+    bool operator()(T&){
+        return false;
+    }
+};
+
+bool use_variable(std::shared_ptr<mtac::BasicBlock> bb, std::shared_ptr<Variable> var){
+    UsageCollector collector(var);
+
+    for(auto& statement : bb->statements){
+        if(visit(collector, statement)){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/*!
+ * \brief Test if an invariant is valid or not. 
+ * An invariant defining v is valid if: 
+ * 1. It is in a basic block that dominates all other uses of v
+ * 2. It is in a basic block that dominates all exit blocks of the loop
+ */
+bool is_valid_invariant(std::shared_ptr<mtac::BasicBlock> source_bb, mtac::Statement statement, const Loop& loop, const G& g, const PredMap& domTreePredMap){
+    auto quadruple = boost::get<std::shared_ptr<mtac::Quadruple>>(statement);
+    auto var = quadruple->result;
+
+    for(auto& vertex : loop){
+        auto bb = g[vertex].block;
+
+        //A bb always dominates itself => no need to consider the source basic block
+        if(bb != source_bb){
+            if(use_variable(bb, var)){
+                if(boost::get(domTreePredMap, vertex) != boost::graph_traits<G>::null_vertex()){
+                    auto dominator = boost::get(domTreePredMap, vertex);
+
+                    //If the bb is not dominated by the source bb, it is not valid
+                    if(g[dominator].block != source_bb){
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    
+    auto exit_block = *loop.rbegin();
+
+    if(g[exit_block].block == source_bb){
+        return true;
+    }
+                
+    if(boost::get(domTreePredMap, exit_block) != boost::graph_traits<G>::null_vertex()){
+        auto dominator = boost::get(domTreePredMap, exit_block);
+
+        //If the exit bb is not dominated by the source bb, it is not valid
+        if(g[dominator].block != source_bb){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+bool loop_invariant_code_motion(const Loop& loop, std::shared_ptr<mtac::Function> function, const G& g, const PredMap& domTreePredMap){
     std::shared_ptr<mtac::BasicBlock> pre_header;
 
     bool optimized = false;
@@ -112,19 +219,19 @@ bool loop_invariant_code_motion(const Loop& loop, std::shared_ptr<mtac::Function
             auto statement = *it;
 
             if(is_invariant(statement, usage)){
-                //TODO Test if the invariant can be moved to the preheader
-                
-                //Create the preheader if necessary
-                if(!pre_header){
-                    pre_header = create_pre_header(loop, function, g);
+                if(is_valid_invariant(bb, statement, loop, g, domTreePredMap)){
+                    //Create the preheader if necessary
+                    if(!pre_header){
+                        pre_header = create_pre_header(loop, function, g);
+                    }
+
+                    it.erase();
+                    pre_header->statements.push_back(statement);
+
+                    optimized = true;
+
+                    continue;
                 }
-                    
-                it.erase();
-                pre_header->statements.push_back(statement);
-
-                optimized = true;
-
-                continue;
             }
 
             ++it;
@@ -147,9 +254,14 @@ bool mtac::loop_invariant_code_motion(std::shared_ptr<mtac::Function> function){
     }
 
     bool optimized = false;
+    
+    std::vector<Vertex> domTreePredVector = std::vector<Vertex>(boost::num_vertices(g), boost::graph_traits<G>::null_vertex());
+    PredMap domTreePredMap = boost::make_iterator_property_map(domTreePredVector.begin(), boost::get(boost::vertex_index, g));
+
+    boost::lengauer_tarjan_dominator_tree(g, boost::vertex(0, g), domTreePredMap);
 
     for(auto& loop : natural_loops){
-        optimized |= ::loop_invariant_code_motion(loop, function, g);
+        optimized |= ::loop_invariant_code_motion(loop, function, g, domTreePredMap);
     }
     
     return optimized;
