60403bcda74a134ff7c29bbb05968804e29a8054
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-09-11 14:00:23 +0200

    Allow template types to be dynamically allocated

--- src/ast/TypeChecker.cpp
+++ src/ast/TypeChecker.cpp
@@ -345,7 +345,7 @@ class CheckerVisitor : public boost::static_visitor<> {
         void operator()(ast::New& new_){
             auto type = visit(ast::TypeTransformer(context), new_.Content->type);
 
-            if(!(type->is_standard_type() || type->is_custom_type())){
+            if(!(type->is_standard_type() || type->is_custom_type() || type->is_template())){
                 throw SemanticalException("Only standard types and struct types can be dynamically allocated", new_.Content->position);
             }
         }
