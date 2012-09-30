9c291a339d9c37559f7cbfe042db3920d4314aa9
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-09-30 22:07:21 +0200

    Add override keyword

--- include/ast/FunctionsAnnotator.hpp
+++ include/ast/FunctionsAnnotator.hpp
@@ -15,7 +15,7 @@ namespace eddic {
 namespace ast {
 
 struct MemberFunctionCollectionPass : Pass {
-    void apply_struct(ast::Struct& struct_, bool indicator);
+    void apply_struct(ast::Struct& struct_, bool indicator) override;
     void apply_struct_function(ast::FunctionDeclaration& function) override;
     void apply_struct_constructor(ast::Constructor& constructor) override;
     void apply_struct_destructor(ast::Destructor& destructor) override;
