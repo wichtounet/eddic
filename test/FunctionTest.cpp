210379fb694c8b14269fe4cf2bb72350944870d8
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-10-11 04:16:26 +0200

    Add new for the creation of bb

--- test/FunctionTest.cpp
+++ test/FunctionTest.cpp
@@ -22,6 +22,9 @@ BOOST_AUTO_TEST_CASE( bb_entry_exit_count ){
 
     BOOST_CHECK_EQUAL(function->entry_bb()->index, -1);
     BOOST_CHECK_EQUAL(function->exit_bb()->index, -2);
+    
+    BOOST_CHECK(function->entry_bb()->next == function->exit_bb());
+    BOOST_CHECK(function->exit_bb()->prev == function->entry_bb());
 
     BOOST_CHECK_EQUAL(function->bb_count(), 2u);
 }
@@ -61,4 +64,31 @@ BOOST_AUTO_TEST_CASE( bb_iterators ){
     BOOST_CHECK_EQUAL((*it)->index, -2);
 
     BOOST_CHECK_EQUAL(function->bb_count(), 5u);
+}
+
+BOOST_AUTO_TEST_CASE( bb_new_bb ){
+    auto function = std::make_shared<mtac::Function>(nullptr, "test_function");
+    
+    BOOST_CHECK_EQUAL(function->bb_count(), 0u);
+
+    function->create_entry_bb();
+    function->create_exit_bb();
+
+    auto bb = function->new_bb();
+    
+    BOOST_CHECK_EQUAL(function->bb_count(), 2u);
+
+    auto it = function->begin();
+    ++it;
+
+    function->insert_before(it, bb);
+    
+    BOOST_CHECK_EQUAL(function->bb_count(), 3u);
+    
+    BOOST_CHECK(bb->next == function->exit_bb());
+    BOOST_CHECK(bb->prev == function->entry_bb());
+    BOOST_CHECK(function->entry_bb()->next == bb);
+    BOOST_CHECK(function->exit_bb()->prev == bb);
+
+
 }
