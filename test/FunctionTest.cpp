e7b595353ac7877cfb4a369e860369152359eba5
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-10-11 04:39:31 +0200

    Test iterators

--- test/FunctionTest.cpp
+++ test/FunctionTest.cpp
@@ -89,6 +89,72 @@ BOOST_AUTO_TEST_CASE( bb_new_bb ){
     BOOST_CHECK(bb->prev == function->entry_bb());
     BOOST_CHECK(function->entry_bb()->next == bb);
     BOOST_CHECK(function->exit_bb()->prev == bb);
+}
 
+BOOST_AUTO_TEST_CASE( bb_remove_bb ){
+    auto function = std::make_shared<mtac::Function>(nullptr, "test_function");
 
+    function->create_entry_bb();
+    auto bb1 = function->append_bb();
+    auto bb2 = function->append_bb();
+    auto bb3 = function->append_bb();
+    auto bb4 = function->append_bb();
+    function->create_exit_bb();
+
+    BOOST_CHECK_EQUAL(function->bb_count(), 6u);
+    
+    function->remove(bb2); 
+    
+    BOOST_CHECK_EQUAL(function->bb_count(), 5u);
+    
+    BOOST_CHECK(bb1->next == bb3);
+    BOOST_CHECK(bb3->prev == bb1);
+
+    auto it = function->begin();
+    ++it;
+    ++it;
+
+    function->remove(it); 
+
+    BOOST_CHECK(bb1->next == bb4);
+    BOOST_CHECK(bb4->prev == bb1);
+
+    BOOST_CHECK_EQUAL(function->bb_count(), 4u);
+}
+
+BOOST_AUTO_TEST_CASE( bb_at ){
+    auto function = std::make_shared<mtac::Function>(nullptr, "test_function");
+    
+    function->create_entry_bb();
+    auto bb1 = function->append_bb();
+    auto bb2 = function->append_bb();
+    function->create_exit_bb();
+
+    auto it = function->begin();
+    ++it;
+    auto it_at = function->at(bb1);
+    BOOST_CHECK(it == it_at);
+    
+    ++it;
+    it_at = function->at(bb2);
+    BOOST_CHECK(it == it_at);
+    
+    ++it;
+    it_at = function->at(function->exit_bb());
+    BOOST_CHECK(it == it_at);
+
+    ++it;
+    ++it_at;
+    BOOST_CHECK(it == function->end());
+    BOOST_CHECK(it_at == function->end());
+
+    auto it_end = function->end();
+   
+    --it_end;
+    --it_at;
+    --it;
+
+    BOOST_CHECK(it == it_at);
+    BOOST_CHECK(it_end == it);
+    BOOST_CHECK(it_at == it_end);
 }
