6e991cc10d017f77dd23f210495f1587f9d9e842
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-10-12 05:59:58 +0200

    Add a file to compute the dominators of each basic block

---
+++ src/mtac/dominators.cpp
@@ -0,0 +1,14 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#include "mtac/dominators.hpp"
+
+using namespace eddic;
+
+void mtac::compute_dominators(std::shared_ptr<Function> function){
+
+}
