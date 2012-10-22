17ae23772f0589196cf3faea3e5fa6ea5dc14eac
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-10-22 02:36:08 +0200

    Fix the direction of the analysis

--- include/ltac/LiveRegistersProblem.hpp
+++ include/ltac/LiveRegistersProblem.hpp
@@ -50,7 +50,7 @@ struct LiveRegisterValues {
 
 std::ostream& operator<<(std::ostream& stream, LiveRegisterValues& expression);
 
-struct LiveRegistersProblem : public mtac::DataFlowProblem<mtac::DataFlowType::Backward, LiveRegisterValues> {
+struct LiveRegistersProblem : public mtac::DataFlowProblem<mtac::DataFlowType::Low_Backward, LiveRegisterValues> {
     ProblemDomain Boundary(std::shared_ptr<mtac::Function> function) override;
     ProblemDomain Init(std::shared_ptr<mtac::Function> function) override;
    
