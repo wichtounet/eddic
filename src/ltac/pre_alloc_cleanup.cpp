90d1c7097bab925db339b28b24ead15e6bc8ff26
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-10-24 21:54:33 +0200

    Add a pre-allocation optimization pass

---
+++ src/ltac/pre_alloc_cleanup.cpp
@@ -0,0 +1,43 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#include "ltac/pre_alloc_cleanup.hpp"
+
+using namespace eddic;
+
+template<typename Variant>
+bool is_pseudo_reg(Variant& var){
+    return boost::get<ltac::PseudoRegister>(&*var);
+}
+
+void ltac::pre_alloc_cleanup(std::shared_ptr<mtac::Program> program){
+    for(auto& function : program->functions){
+        for(auto& bb : function){
+            auto it = iterate(bb->l_statements);
+
+            while(it.has_next()){
+                auto statement = *it;
+
+                if(auto* ptr = boost::get<std::shared_ptr<ltac::Instruction>>(&statement)){
+                    auto instruction = *ptr;
+
+                    if(instruction->op == ltac::Operator::MOV && is_pseudo_reg(instruction->arg1) && is_pseudo_reg(instruction->arg2)){
+                        auto reg1 = boost::get<ltac::PseudoRegister>(*instruction->arg1);
+                        auto reg2 = boost::get<ltac::PseudoRegister>(*instruction->arg2);
+
+                        if(reg1 == reg2){
+                            it.erase();
+                            continue;
+                        }
+                    }
+                }
+
+                ++it;
+            }
+        }
+    }
+}
