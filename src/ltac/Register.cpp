d7dc238f388a9f34110530422c2ca55b26e6267b
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-10-26 03:11:43 +0200

    Complete printing of SP/BP registers

--- src/ltac/Register.cpp
+++ src/ltac/Register.cpp
@@ -38,5 +38,11 @@ bool ltac::Register::operator!=(const Register& rhs) const {
 }
 
 std::ostream& ltac::operator<<(std::ostream& out, const ltac::Register& reg){
+    if(reg == ltac::BP){
+        return out << "bp";
+    } else if(reg == ltac::SP){
+        return out << "sp";
+    }
+
     return out << "r" << reg.reg;
 }
