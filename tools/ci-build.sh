b065fbda0f9c622ce15773a829357d227070ed47
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-11-21 21:46:39 -0800

    Fix typo

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -2,7 +2,7 @@ export CCACHE_DIR=/var/tmp/ccache/
 cmake -DCMAKE_CXX_COMPILER=/usr/lib/ccache/bin/clang++ -DCMAKE_C_COMPILER=/usr/lib/ccache/bin/clang .
 make
 
-echo "Run the test"
+echo "Run the test suite"
 ./bin/boosttest--eddic_boost_test --report_level=detailed --report_format=xml --report_sink=test-results.xml
 
 echo "Generate the doc"
