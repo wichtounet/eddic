3174d69ddcc146829b95b47e603d76db6d66d8c6
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-11-21 21:44:18 -0800

    Improve the CI Build script

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -1,5 +1,14 @@
 export CCACHE_DIR=/var/tmp/ccache/
 cmake -DCMAKE_CXX_COMPILER=/usr/lib/ccache/bin/clang++ -DCMAKE_C_COMPILER=/usr/lib/ccache/bin/clang .
 make
+
+echo "Run the test"
 ./bin/boosttest--eddic_boost_test --report_level=detailed --report_format=xml --report_sink=test-results.xml
+
+echo "Generate the doc"
 make doc
+
+echo "Compress the doc"
+cd doc/html/
+tar cvzf doc.tar.gz *
+mv doc.tar.gz ../../
