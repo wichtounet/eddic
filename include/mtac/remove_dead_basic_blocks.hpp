6942df6a0e09f3af28e7580999a62677b6cd6f5a
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-12-22 21:31:35 -0800

    Split MTAC headers

--- include/mtac/remove_dead_basic_blocks.hpp
+++ include/mtac/remove_dead_basic_blocks.hpp
@@ -5,8 +5,8 @@
 //  http://www.boost.org/LICENSE_1_0.txt)
 //=======================================================================
 
-#ifndef MTAC_MERGE_BASIC_BLOCKS_H
-#define MTAC_MERGE_BASIC_BLOCKS_H
+#ifndef MTAC_REMOVE_DEAD_BLOCKS_H
+#define MTAC_REMOVE_DEAD_BLOCKS_H
 
 #include <memory>
 
@@ -16,18 +16,6 @@
 namespace eddic {
 
 namespace mtac {
-
-struct merge_basic_blocks {
-    bool operator()(mtac::function_p function);
-};
-
-template<>
-struct pass_traits<merge_basic_blocks> {
-    STATIC_CONSTANT(pass_type, type, pass_type::CUSTOM);
-    STATIC_STRING(name, "merge_bb");
-    STATIC_CONSTANT(unsigned int, property_flags, 0);
-    STATIC_CONSTANT(unsigned int, todo_after_flags, TODO_REMOVE_NOP);
-};
 
 struct remove_dead_basic_blocks {
     bool operator()(mtac::function_p function);
