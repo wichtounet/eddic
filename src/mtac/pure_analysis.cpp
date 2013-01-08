f9af6503e7d564ff4a933f2e4dd69e03ffe211a8
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-01-07 20:39:48 -0800

    Add future directions

--- src/mtac/pure_analysis.cpp
+++ src/mtac/pure_analysis.cpp
@@ -11,6 +11,14 @@ using namespace eddic;
 
 bool mtac::pure_analysis::operator()(mtac::Program& program){
     //TODO
+    //For each function
+    // if a parameter is a pointer => function not pure
+    // if a global variable is modified => function not pure
+    // if a not pure function is called => function not pure
+    // if a function not analyzed is called => delay 
+    //
+    // iterate as long as there as more analyzer function
+    // if remaining functions => put them not pure
 
     //It is only an analysis pass, it is not necessary to restart the other passes
     return false;
