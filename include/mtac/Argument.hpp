be6f0dafb0e51b833b369e7c968866137f6bbbc3
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-01-19 13:40:58 -0800

    Cleanup

--- include/mtac/Argument.hpp
+++ include/mtac/Argument.hpp
@@ -10,72 +10,12 @@
 
 #include <memory>
 #include <string>
-#include <ostream>
 
 #include "variant.hpp"
 
-#define CUSTOM_STRONG_TYPEDEF(T, D)                                         \
-struct D {                                                                  \
-    T t;                                                                    \
-    explicit D(const T t_) : t(t_) {};                                      \
-                                                                            \
-    D(){};                                                                  \
-    D(const D & t_) : t(t_.t){}                                             \
-                                                                            \
-    D(int t_) : t(t_) {}                                                    \
-    D(unsigned int t_) : t((int) t_) {}                                     \
-    D(double t_) : t(t_) {}                                                 \
-    D(const std::string& t_) : t(t_) {}                                     \
-    D(std::shared_ptr<Variable> t_) : t(t_) {}                              \
-                                                                            \
-    D& operator=(const D & rhs) { t = rhs.t; return *this;}                 \
-    D& operator=(const T & rhs) { t = rhs; return *this;}                   \
-                                                                            \
-    D& operator=(int rhs) { t = rhs; return *this; }                        \
-    D& operator=(unsigned int rhs) { t = (int) rhs; return *this; }         \
-    D& operator=(double rhs) { t = rhs; return *this; }                     \
-    D& operator=(const std::string& rhs) { t = rhs; return *this; }         \
-    D& operator=(std::shared_ptr<Variable> rhs) { t = rhs; return *this; }  \
-    operator const T &() const {return t; }                                 \
-    operator T &() { return t; }                                            \
-                                                                            \
-    T* int_ptr() {return &t; }                                              \
-    const T* int_ptr() const  {return &t; }                                 \
-                                                                            \
-    T& int_ref() {return t; }                                               \
-    const T& int_ref() const {return t; }                                   \
-                                                                            \
-    template<typename Visitor>                                              \
-    typename Visitor::result_type apply_visitor(Visitor& visitor)           \
-    { return int_ref().apply_visitor(visitor); }                            \
-};
-
 namespace eddic {
+
 class Variable;
-} //end of eddic
-
-namespace eddi_detail {
-
-    struct equals_visitor : boost::static_visitor<bool> {
-        template <typename T>
-        bool operator()(const T& a, const T& b) const
-        { return a == b; }
-
-        template<typename T, typename U>
-        bool operator()(const T&, const U&) const
-        { return false; }
-    };
-
-    struct variant_equals {
-        equals_visitor _helper;
-
-        template<typename variant_t>
-        bool operator()(const variant_t& a, const variant_t& b) const
-        { return boost::apply_visitor(_helper, a, b); }
-    };
-} //end of eddi_detail
-
-namespace eddic {
 
 namespace mtac {
 
