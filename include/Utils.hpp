7025f0981fabca7b9dcae922044a40fb110e3535
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-02-06 08:50:10 -0800

    Remove old unused function

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -41,24 +41,6 @@ bool are_equals(const std::vector<T>& a, const std::vector<T>& b){
 }
 
 /*!
- * \brief Find the index of an element inside a vector. 
- * \param vector The vector to search into.   
- * \param search The element to search into the vector.   
- * \return The index of the element in the vector..
- * \throw runtime_error If the element is not contained in the vector. 
- */
-template<typename T>
-unsigned int index(const std::vector<T>& vector, T& search){
-    for(unsigned int i = 0; i < vector.size(); ++i){
-        if(vector[i] == search){
-            return i;
-        }
-    }
-
-    throw std::runtime_error("The element is not contained in the vector");
-}
-
-/*!
  * \brief Convert a string to a number of an arbitrary type. 
  * \param text The string to convert.   
  * \return The converted text in the good type.
