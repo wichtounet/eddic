08b192949e548848e42162ea24471fa4c3e0b697
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-02-10 17:12:21 -0800

    Document loop

--- include/mtac/loop.hpp
+++ include/mtac/loop.hpp
@@ -39,23 +39,75 @@ struct LinearEquation {
 
 typedef std::map<std::shared_ptr<Variable>, LinearEquation> InductionVariables;
 
+/*!
+ * \brief Defines a loop in MTAC IR. A loop is made of a set of basic blocks.
+ */
 class loop {
     public:
         typedef std::set<mtac::basic_block_p>::iterator iterator;
 
+        /*!
+         * \brief Construct a new loop
+         * \param blocks The set of basic blocks inside the loop
+         */
         loop(const std::set<mtac::basic_block_p>& blocks);
 
+        /*!
+         * \brief Return an iterator to the first basic block of the loop. 
+         * \return an iterator to the first basic block of the loop.
+         */
         iterator begin();
+
+        /*!
+         * \brief Return an iterator one past the last basic block of the loop. 
+         * \return an iterator one past the last basic block of the loop.
+         */
         iterator end();
         
+        /*!
+         * \brief The set of basic blocks forming the loop. 
+         * \return The set of basic blocks forming the loop. 
+         */
         std::set<mtac::basic_block_p>& blocks();
+        
+        /*!
+         * \brief The set of basic blocks forming the loop. 
+         * \return The set of basic blocks forming the loop. 
+         */
         const std::set<mtac::basic_block_p>& blocks() const;
 
+        /*!
+         * \brief Indicate if the loop analysis has been able to estimate the number of iterations of the loop. 
+         * \return true if the estimation of the loop is made, false otherwise.
+         */
         bool has_estimate();
+
+        /*!
+         * \brief Return the number of times the loop iterates. 
+         *
+         * If has_estimate() returns false, this function may return any value. 
+         * \return The number of iterations of the loop. 
+         */
         long& estimate();
+
+        /*!
+         * \brief Return the initial value of the basic induction variable that is used to iterate. 
+         *
+         * If has_estimate() returns false, this function may return any value. 
+         * \return The initial value of the basic induction variable.
+         */
         long& initial_value();
 
+        /*!
+         * \brief Return the list of basic induction variables of the loop. 
+         * \return the list of basic induction variables of the loop. 
+         */
         InductionVariables& basic_induction_variables();
+
+        /*!
+         * \brief Return the list of dependent induction variables of the loop. 
+         * \return the list of dependent induction variables of the loop. 
+         */
         InductionVariables& dependent_induction_variables();
 
         /*!
