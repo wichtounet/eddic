deec898d3ce36b0ba8d4595ee279c3692896cbe7
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-06-05 21:23:35 +0200

    Cleanup

--- src/parser/main.cpp
+++ src/parser/main.cpp
@@ -2,19 +2,18 @@
 #include "ast/SourceFile.hpp"
 #include "parser/SpiritParser.hpp"
 
-int main(int argc, char* args[])
-{
+int main(int argc, char* args[]){
     const std::vector<std::string> argv(args+1, args+argc);
 
     using namespace eddic;
     using namespace eddic::parser;
 
-    for (int i=0; i<100; i++)
-        for (auto& fname : argv)
-        {
+    for (int i=0; i<100; i++){
+        for (auto& fname : argv){
             SpiritParser parser;
 
             eddic::ast::SourceFile program;
             parser.parse(fname, program, std::make_shared<GlobalContext>(Platform::INTEL_X86_64));
         }
+    }
 }
