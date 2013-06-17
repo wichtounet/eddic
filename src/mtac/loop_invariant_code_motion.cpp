6c0fe3b9ef133dd294f6e7a80ba2ec6edbc1b060
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-06-17 16:59:20 +0200

    Improve loop invariant code motion for bigger loops

--- src/mtac/loop_invariant_code_motion.cpp
+++ src/mtac/loop_invariant_code_motion.cpp
@@ -51,15 +51,20 @@ bool is_invariant(mtac::Quadruple& quadruple, mtac::Usage& usage){
     return false;
 }
 
+bool is_arithmetic_expression(mtac::Operator op){
+    return op >= mtac::Operator::ADD && op <= mtac::Operator::FDIV;
+}
+
+
 /*!
- * \brief Test if an invariant is valid or not. 
- * An invariant defining v is valid if: 
+ * \brief Test if an invariant is valid or not.
+ * An invariant defining v is valid if:
  * 1. It is in a basic block that dominates all other uses of v
  * 2. It is in a basic block that dominates all exit blocks of the loop
  * 3. It is not an NOP
  */
 bool is_valid_invariant(mtac::basic_block_p source_bb, mtac::Quadruple& quadruple, mtac::loop& loop){
-    //It is not necessary to move statements with no effects. 
+    //It is not necessary to move statements with no effects.
     if(quadruple.op == mtac::Operator::NOP){
         return false;
     }
@@ -79,20 +84,26 @@ bool is_valid_invariant(mtac::basic_block_p source_bb, mtac::Quadruple& quadrupl
             }
         }
     }
-    
-    auto exit_block = *loop.blocks().rbegin();
+
+    //An arithmetic expression has no side effects and so the last condition can
+    //be relaxed
+    if(is_arithmetic_expression(quadruple.op)){
+        return true;
+    }
+
+    auto exit_block = loop.find_exit();
 
     if(exit_block == source_bb){
         return true;
     }
-                
+
     auto dominator = exit_block->dominator;
 
     //If the exit bb is not dominated by the source bb, it is not valid
     if(dominator != source_bb){
         return false;
     }
-    
+
     return true;
 }
 
@@ -106,7 +117,11 @@ bool loop_invariant_code_motion(mtac::loop& loop, mtac::Function& function){
     for(auto& bb : loop){
         for(auto& statement : bb->statements){
             if(is_invariant(statement, usage)){
+                std::cout << "Found invariant " << statement << std::endl;
+
                 if(is_valid_invariant(bb, statement, loop)){
+                    std::cout << "Found valid invariant " << statement << std::endl;
+
                     //Create the preheader if necessary
                     if(!pre_header){
                         pre_header = loop.find_safe_preheader(function, true);
@@ -118,7 +133,7 @@ bool loop_invariant_code_motion(mtac::loop& loop, mtac::Function& function){
                     mtac::transform_to_nop(statement);
 
                     optimized = true;
-                } 
+                }
             }
         }
     }
@@ -136,10 +151,8 @@ bool mtac::loop_invariant_code_motion::operator()(mtac::Function& function){
     bool optimized = false;
 
     for(auto& loop : function.loops()){
-        if(loop.blocks().size() == 1){
-            optimized |= ::loop_invariant_code_motion(loop, function);
-        }
+        optimized |= ::loop_invariant_code_motion(loop, function);
     }
-    
+
     return optimized;
 }
