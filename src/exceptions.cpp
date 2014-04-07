c9e231bf5d54a4851513a0f29b5af3f8fc9d6153
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-04-07 23:39:57 +0200

    Fix glitch with Clang and boost

---
+++ src/exceptions.cpp
@@ -0,0 +1,16 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2011-2013.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#include <iostream>
+
+namespace boost {
+
+void throw_exception(std::exception const & e){
+    throw e;
+}
+
+} // namespace boost
