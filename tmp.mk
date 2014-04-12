22fe89e580043dcca98158a238a5dc8838b966ae
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-04-12 16:31:18 +0200

    Fix compilation and add link steps

--- tmp.mk
+++ tmp.mk
@@ -2,14 +2,16 @@ default: release
 
 .PHONY: default release debug all clean
 
+BOOST_PREFIX=$(HOME)/build/modular-boost/
+
 TEST_EXE=bin/boosttest--eddic_boost_test
 
 CC=clang++
 LD=clang++
 
 WARNING_FLAGS=-Wextra -Wall -Qunused-arguments -Wuninitialized -Wsometimes-uninitialized -Wno-long-long -Winit-self -Wdocumentation -pedantic
-CXX_FLAGS=-Iinclude -std=c++1y -stdlib=libc++ $(WARNING_FLAGS)
-LD_FLAGS=$(CXX_FLAGS)
+CXX_FLAGS=-Iinclude -std=c++1y -stdlib=libc++ $(WARNING_FLAGS) -isystem $(BOOST_PREFIX)/include
+LD_FLAGS=$(CXX_FLAGS) -L $(BOOST_PREFIX)/lib -lboost_program_options
 
 DEBUG_FLAGS=-g
 RELEASE_FLAGS=-g -DLOGGING_DISABLE -DNDEBUG -Ofast -march=native -fvectorize -fslp-vectorize-aggressive -fomit-frame-pointer
@@ -17,22 +19,27 @@ RELEASE_2_FLAGS=$(RELEASE_FLAGS) -fno-exceptions -fno-rtti
 
 # Search the source files
 
-CPP_FILES=$(wildcard src/*.cpp)
-CPP_FILES_AST=$(wildcard src/ast/*.cpp)
-CPP_FILES_ASM=$(wildcard src/asm/*.cpp)
-CPP_FILES_LTAC=$(wildcard src/ltac/*.cpp)
-CPP_FILES_MTAC=$(wildcard src/mtac/*.cpp)
-CPP_FILES_LEXER=$(wildcard src/lexer/*.cpp)
-CPP_FILES_PARSER=$(wildcard src/parser/*.cpp)
+SRC_CPP_FILES=$(wildcard src/*.cpp)
+SRC_CPP_FILES_AST=$(wildcard src/ast/*.cpp)
+SRC_CPP_FILES_ASM=$(wildcard src/asm/*.cpp)
+SRC_CPP_FILES_LTAC=$(wildcard src/ltac/*.cpp)
+SRC_CPP_FILES_MTAC=$(wildcard src/mtac/*.cpp)
+SRC_CPP_FILES_LEXER=$(wildcard src/lexer/*.cpp)
+SRC_CPP_FILES_PARSER=$(wildcard src/parser/*.cpp)
 
-CPP_FILES_ALL=$(CPP_FILES) $(CPP_FILES_AST) $(CPP_FILES_ASM) $(CPP_FILES_LTAC) $(CPP_FILES_MTAC) $(CPP_FILES_LEXER) $(CPP_FILES_PARSER)
+SRC_CPP_FILES_ALL=$(SRC_CPP_FILES) $(SRC_CPP_FILES_AST) $(SRC_CPP_FILES_ASM) $(SRC_CPP_FILES_LTAC) $(SRC_CPP_FILES_MTAC) $(SRC_CPP_FILES_LEXER) $(SRC_CPP_FILES_PARSER)
 
-CPP_FILES_NON_EXEC := $(filter-out src/eddi.cpp,$(CPP_FILES_ALL))
-CPP_FILES_NON_EXEC := $(filter-out src/parser/main.cpp,$(CPP_FILES_NON_EXEC))
-CPP_FILES_NON_EXEC := $(filter-out src/lexer/main.cpp,$(CPP_FILES_NON_EXEC))
+SRC_CPP_FILES_NON_EXEC := $(filter-out src/eddi.cpp,$(SRC_CPP_FILES_ALL))
+SRC_CPP_FILES_NON_EXEC := $(filter-out src/parser/main.cpp,$(SRC_CPP_FILES_NON_EXEC))
+SRC_CPP_FILES_NON_EXEC := $(filter-out src/lexer/main.cpp,$(SRC_CPP_FILES_NON_EXEC))
 
-DEBUG_O_FILES_NON_EXEC=$(NONEXEC_CPP_FILES:%.cpp=debug/%.cpp.o)
-RELEASE_O_FILES_NON_EXEC=$(NONEXEC_CPP_FILES:%.cpp=release/%.cpp.o)
+DEBUG_O_FILES_NON_EXEC=$(SRC_CPP_FILES_NON_EXEC:%.cpp=debug/%.cpp.o)
+RELEASE_O_FILES_NON_EXEC=$(SRC_CPP_FILES_NON_EXEC:%.cpp=release/%.cpp.o)
+
+TEST_CPP_FILES=$(wildcard test/*.cpp)
+
+DEBUG_TEST_O_FILES_NON_EXEC=$(TEST_CPP_FILES:%.cpp=debug/%.cpp.o)
+RELEASE_TEST_O_FILES_NON_EXEC=$(TEST_CPP_FILES:%.cpp=release/%.cpp.o)
 
 # Actual compilation of all the files
 
@@ -60,7 +67,7 @@ release/src/lexer/%.cpp.o: src/lexer/%.cpp
 	@ mkdir -p release/src/lexer/
 	$(CC) $(CXX_FLAGS) $(RELEASE_FLAGS) -o $@ -c $<
 
-debug/src/paser/%.cpp.o: src/parser/%.cpp
+debug/src/parser/%.cpp.o: src/parser/%.cpp
 	@ mkdir -p debug/src/parser/
 	$(CC) $(CXX_FLAGS) $(DEBUG_FLAGS) -o $@ -c $<
 
@@ -69,24 +76,62 @@ release/src/parser/%.cpp.o: src/parser/%.cpp
 	$(CC) $(CXX_FLAGS) $(RELEASE_FLAGS) -o $@ -c $<
 
 debug/src/asm/%.cpp.o: src/asm/%.cpp
-	@ mkdir -p debug/src/parser/
+	@ mkdir -p debug/src/asm/
 	$(CC) $(CXX_FLAGS) $(DEBUG_FLAGS) -o $@ -c $<
 
 release/src/asm/%.cpp.o: src/asm/%.cpp
 	@ mkdir -p release/src/asm/
 	$(CC) $(CXX_FLAGS) $(RELEASE_2_FLAGS) -o $@ -c $<
 
-debug/src/ltac/%.cpp.o: src/ltac/%.cpp
-	@ mkdir -p debug/src/ltac/
+debug/src/mtac/%.cpp.o: src/mtac/%.cpp
+	@ mkdir -p debug/src/mtac/
 	$(CC) $(CXX_FLAGS) $(DEBUG_FLAGS) -o $@ -c $<
 
 release/src/mtac/%.cpp.o: src/mtac/%.cpp
 	@ mkdir -p release/src/mtac/
 	$(CC) $(CXX_FLAGS) $(RELEASE_2_FLAGS) -o $@ -c $<
 
+debug/src/ltac/%.cpp.o: src/ltac/%.cpp
+	@ mkdir -p debug/src/ltac/
+	$(CC) $(CXX_FLAGS) $(DEBUG_FLAGS) -o $@ -c $<
+
+release/src/ltac/%.cpp.o: src/ltac/%.cpp
+	@ mkdir -p release/src/ltac/
+	$(CC) $(CXX_FLAGS) $(RELEASE_2_FLAGS) -o $@ -c $<
+
+debug/test/%.cpp.o: test/%.cpp
+	@ mkdir -p debug/test/
+	$(CC) $(CXX_FLAGS) $(DEBUG_FLAGS) -o $@ -c $<
+
+release/test/%.cpp.o: test/%.cpp
+	@ mkdir -p release/test/
+	$(CC) $(CXX_FLAGS) $(RELEASE_2_FLAGS) -o $@ -c $<
+
 # Link the various binaries
 
-# TODO
+debug/bin/eddic: debug/src/eddi.cpp.o $(DEBUG_O_FILES_NON_EXEC)
+	@ mkdir -p debug/bin/
+	$(LD) $(CXX_FLAGS) $(DEBUG_FLAGS) -o $@ $? $(LD_FLAGS)
+
+release/bin/eddic: release/src/eddi.cpp.o $(RELEASE_O_FILES_NON_EXEC)
+	@ mkdir -p release/bin/
+	$(LD) $(LD_FLAGS) $(RELEASE_FLAGS) -o $@ $?
+
+debug/bin/time_parse: debug/src/parser/main.cpp.o $(DEBUG_O_FILES_NON_EXEC)
+	@ mkdir -p debug/bin/
+	$(LD) $(LD_FLAGS) $(DEBUG_FLAGS) -o $@ $?
+
+release/bin/time_parse: release/src/parser/main.cpp.o $(RELEASE_O_FILES_NON_EXEC)
+	@ mkdir -p release/bin/
+	$(LD) $(LD_FLAGS) $(RELEASE_FLAGS) -o $@ $?
+
+debug/bin/generate_lexer: debug/src/lexer/main.cpp.o $(DEBUG_O_FILES_NON_EXEC)
+	@ mkdir -p debug/bin/
+	$(LD) $(LD_FLAGS) $(DEBUG_FLAGS) -o $@ $?
+
+release/bin/generate_lexer: release/src/lexer/main.cpp.o $(RELEASE_O_FILES_NON_EXEC)
+	@ mkdir -p release/bin/
+	$(LD) $(LD_FLAGS) $(RELEASE_FLAGS) -o $@ $?
 
 # Generate the dependency files
 
