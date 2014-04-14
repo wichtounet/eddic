f0748af952c37f818ba7171fe76ef636c45f5c95
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-04-14 23:09:39 +0200

    Update copyright

--- src/mtac/ConstantFolding.cpp
+++ src/mtac/ConstantFolding.cpp
@@ -1,5 +1,5 @@
 //=======================================================================
-// Copyrhs Baptiste Wicht 2011-2013.
+// Copyrhs Baptiste Wicht 2011-2014.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
