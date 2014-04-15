889a88b84cded8e86cb97d72d048095d196f94a4
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-04-15 17:23:46 +0200

    Switch to MIT License

--- include/mtac/pass_traits.hpp
+++ include/mtac/pass_traits.hpp
@@ -1,8 +1,8 @@
 //=======================================================================
 // Copyright Baptiste Wicht 2011-2014.
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
-//  http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
 //=======================================================================
 
 #ifndef MTAC_PASS_TRAITS_H
