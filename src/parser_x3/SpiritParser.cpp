34b28ae241d9a4ebddc1d4672445fc6a30aee276
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-04-16 22:51:14 +0200

    Add support for position

--- src/parser_x3/SpiritParser.cpp
+++ src/parser_x3/SpiritParser.cpp
@@ -13,8 +13,13 @@
 #include <string>
 
 //#include "boost_cfg.hpp"
+
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/count.hpp>
+
 #include <boost/spirit/home/x3.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/spirit/include/classic_position_iterator.hpp>
 
 //#include "GlobalContext.hpp"
 
@@ -23,6 +28,31 @@
 namespace x3 = boost::spirit::x3;
 
 using namespace eddic;
+
+template<typename ForwardIteratorT>
+class extended_iterator : public boost::spirit::classic::position_iterator2<ForwardIteratorT> {
+    std::size_t current_file;
+
+public:
+    extended_iterator() : boost::spirit::classic::position_iterator2<ForwardIteratorT>() {
+        //Nothing
+    }
+
+    template <typename FileNameT>
+    extended_iterator(
+        const ForwardIteratorT& begin, const ForwardIteratorT& end,
+        FileNameT file, std::size_t current_file)
+            : boost::spirit::classic::position_iterator2<ForwardIteratorT>(begin, end, file), current_file(current_file)
+    {}
+
+    std::size_t get_current_file() const {
+        return current_file;
+    }
+
+    extended_iterator(const extended_iterator& iter) = default;
+    extended_iterator& operator=(const extended_iterator& iter) = default;
+};
+
 
 namespace x3_ast {
 
@@ -33,6 +63,7 @@ struct position {
 };
 
 struct function_declaration {
+    position pos;
     std::string type;
     std::string name;
 };
@@ -98,6 +129,18 @@ namespace x3_grammar {
     x3::rule<standard_import_id, x3_ast::standard_import> const standard_import("standard_import");
     x3::rule<import_id, x3_ast::import> const import("import");
 
+    typedef boost::mpl::vector<import_id, standard_import_id, function_declaration_id> annotated_ids;
+    typedef boost::mpl::vector<x3_ast::import, x3_ast::standard_import, x3_ast::function_declaration>::type annotated_asts;
+
+    template <typename iterator_type, typename Attr, typename Context>
+    inline void on_success(standard_import_id, const iterator_type& first, const iterator_type&, Attr& attr, Context const&){
+        auto& pos = first.get_position();
+
+        attr.pos.file = first.get_current_file();
+        attr.pos.line = pos.line;
+        attr.pos.column = pos.column;
+    }
+
     auto const standard_import_def = 
             x3::attr(1)
         >>  "import"
@@ -151,19 +194,30 @@ bool parser_x3::SpiritParser::parse(const std::string& file/*, ast::SourceFile& 
     auto& parser = x3_grammar::parser;
 
     x3_ast::source_file result;
-    auto it = file_contents.begin();
-    auto end = file_contents.end();
     boost::spirit::x3::ascii::space_type space;
 
-    bool r = x3::phrase_parse(it, end, parser, space, result);
+    typedef std::string::iterator base_iterator_type;
+    //typedef boost::spirit::classic::position_iterator2<base_iterator_type> pos_iterator_type;
+    typedef extended_iterator<base_iterator_type> pos_iterator_type;
 
-    if(r && it == end){
-        std::cout << "Blocks: " << result.blocks.size() << std::endl;
+    pos_iterator_type it(file_contents.begin(), file_contents.end(), file, 1);
+    pos_iterator_type end;
 
+    try {
+        bool r = x3::phrase_parse(it, end, parser, space, result);
 
-        return true;
-    } else {
+        if(r && it == end){
+            std::cout << "Blocks: " << result.blocks.size() << std::endl;
 
+            return true;
+        } else {
+            //TODO
+            std::cout << std::string(it, end) << std::endl;
+
+            return false;
+        }
+    } catch(const boost::spirit::x3::expectation_failure<pos_iterator_type>& e){
+        //TODO
         return false;
     }
 }
