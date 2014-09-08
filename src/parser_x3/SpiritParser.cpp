f45a4625314f536c350ce0cfcfbf903e634f7430
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-09-08 22:48:45 +0200

    Tests...

--- src/parser_x3/SpiritParser.cpp
+++ src/parser_x3/SpiritParser.cpp
@@ -160,8 +160,8 @@ struct call_operation_value {
 };
 
 typedef x3::variant<
-        std::string,
         value,
+        std::string,
         call_operation_value
     >  operation_value;
 
@@ -1185,6 +1185,9 @@ namespace x3_grammar {
     struct assignment_expression_class;
 
     struct bracket_operation_class;
+    struct dot_operation_class;
+    struct call_operation_class;
+    struct postfix_operation_class;
 
     typedef x3::identity<struct instruction> instruction_id;
     struct foreach_class;
@@ -1251,6 +1254,9 @@ namespace x3_grammar {
     x3::rule<primary_value_class, x3_ast::value> const primary_value("primary_value");
     
     x3::rule<bracket_operation_class, x3_ast::operation> const bracket_operation("bracket_operation");
+    x3::rule<dot_operation_class, x3_ast::operation> const dot_operation("dot_operation");
+    x3::rule<call_operation_class, x3_ast::operation> const call_operation("call_operation");
+    x3::rule<postfix_operation_class, x3_ast::operation> const postfix_operation("postfix_operation");
 
     x3::rule<instruction_id, x3_ast::instruction> const instruction("instruction");
     x3::rule<foreach_class, x3_ast::foreach> const foreach("foreach");
@@ -1313,6 +1319,9 @@ namespace x3_grammar {
     struct conditional_expression_class {};
 
     struct bracket_operation_class {};
+    struct dot_operation_class {};
+    struct call_operation_class {};
+    struct postfix_operation_class {};
 
     struct function_parameter_class : annotation_base {};
     struct template_function_declaration_class : annotation_base {};
@@ -1481,10 +1490,31 @@ namespace x3_grammar {
          >>  ']'
          ;
 
+    auto const call_operation_def = 
+             '.'
+         >>  x3::attr(ast::Operator::CALL)
+         >>  call_operation_value
+         ;
+
+    auto const dot_operation_def = 
+             '.'
+         >>  x3::attr(ast::Operator::DOT)
+         >>  identifier
+         ;
+
+    auto const postfix_operation_def = 
+             postfix_op
+         >>  x3::attr(x3_ast::false_())
+         ;
+
     auto const postfix_expression_def =
             primary_value
         >>  +(
-                bracket_operation
+                    bracket_operation
+                |   call_operation
+                //|   postfix_operation
+
+                //|   postfix_operation
                          //'['
                      //>>  x3::attr(ast::Operator::BRACKET)
                      //>>  value
@@ -1600,7 +1630,10 @@ namespace x3_grammar {
         assignment = assignment_def,
         assignment_expression = assignment_expression_def,
 
-        bracket_operation = bracket_operation_def
+        bracket_operation = bracket_operation_def,
+        postfix_operation = postfix_operation_def,
+        call_operation = call_operation_def,
+        dot_operation = dot_operation_def
     );
 
     auto const value_grammar = x3::skip(skipper)[value];
