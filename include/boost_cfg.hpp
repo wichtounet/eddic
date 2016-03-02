59e472271629991998ddfc433ee544a060ae6edc
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-03-02 08:44:12 +0100

    Fix exceptions and boost

--- include/boost_cfg.hpp
+++ include/boost_cfg.hpp
@@ -8,8 +8,16 @@
 #ifndef EDDIC_BOOST_CFG_H
 #define EDDIC_BOOST_CFG_H
 
+#include <exception>
+
+namespace boost {
+
+inline void throw_exception(const std::exception&){}
+
+} // namespace boost
+
 #define EDDIC_BOOST_LIMIT_SIZE 30
-#define BOOST_MPL_CFG_NO_PREPROCESSED_HEADERS 
+#define BOOST_MPL_CFG_NO_PREPROCESSED_HEADERS
 #define BOOST_MPL_LIMIT_VECTOR_SIZE EDDIC_BOOST_LIMIT_SIZE
 #define BOOST_MPL_LIMIT_LIST_SIZE EDDIC_BOOST_LIMIT_SIZE
 
